76a0446289305e429d21962d586a6753f8891047
Sorami Hisamoto <hi@sorami.dev>
2023-11-08 14:13:32 +0900

    tauri-api in svelte

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,3 +1,26 @@
-<div class="text-center">
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+	async function mierune() {
+		greetMsg = await invoke('mierune');
+	}
+</script>
+
+<div class="text-center flex flex-col gap-12 my-12">
 	<h1 class="text-2xl font-bold text-red-700">Hello MIERUNE</h1>
+
+	<div>
+		<input class="border-4" id="greet-input" placeholder="Enter a name..." bind:value={name} />
+		<button on:click={greet}>Greet</button>
+		<p>{greetMsg}</p>
+	</div>
+	<div>
+		<button on:click={mierune}>MIERUNE</button>
+	</div>
 </div>
