7462fef1b28f02370f9cd5acdb6f2afbd2801867
nokonoko1203 <healtheworld.1203@gmail.com>
2023-11-08 23:18:39 +0900

    convert to Geometry

--- nusamai-czml/src/lib.rs
+++ nusamai-czml/src/lib.rs
@@ -26,37 +26,48 @@ fn load_geojson(filepath: String) -> String {
 fn convert_to_geometry(geojson: &str) -> Result<Geometry, serde_json::Error> {
     let value: Value = serde_json::from_str(geojson)?;
 
-    let geometry = &value["features"][0]["geometry"];
-    let geometry_type = geometry["type"].as_str();
-    let coordinates = geometry["coordinates"].as_array();
+    let features = &value["features"].as_array().unwrap();
+
+    let geom_list = features
+        .iter()
+        .map(|x| x["geometry"].clone())
+        .collect::<Vec<_>>();
 
     let mut vertices: Vec<Vec<f64>> = Vec::new();
     let mut indices: Vec<Vec<u32>> = Vec::new();
 
-    for polygon in coordinates.unwrap() {
-        let polygon_vertices = polygon.as_array().unwrap();
-        for vertex in polygon_vertices {
-            let vertex_coordinates = vertex.as_array().unwrap();
-
-            let mut v = Vec::new();
-            for coordinate in vertex_coordinates {
-                let coordinate_f64 = coordinate.as_f64().unwrap();
-                v.push(coordinate_f64);
+    for geom in geom_list {
+        let coordinates = geom["coordinates"].as_array();
+        for polygon in coordinates.unwrap() {
+            let polygon_vertices = polygon.as_array().unwrap();
+            for vertex in polygon_vertices {
+                let vertex_coordinates = vertex.as_array().unwrap();
+                let mut v = Vec::new();
+                for coordinate in vertex_coordinates {
+                    let coordinate_f64 = coordinate.as_f64().unwrap();
+                    v.push(coordinate_f64);
+                }
+                vertices.push(v);
             }
-            vertices.push(v);
-        }
-        let mut i = (0..vertices.len() as u32).collect::<Vec<_>>();
-        let max = indices.iter().map(|x| indices.len()).max().unwrap_or(0) as u32;
+            let mut i = (0..vertices.len() as u32).collect::<Vec<_>>();
+            println!("i: {:?}\n", i);
 
-        i = i.iter().map(|x| x + max).collect::<Vec<_>>();
-        indices.push(i);
+            let max = indices.iter().flatten().max();
+            println!("max: {:?}\n", max);
+            if max.is_none() {
+                indices.push(i);
+                continue;
+            }
+            i = i.iter().map(|x| x + max.unwrap() + 1).collect::<Vec<_>>();
+            indices.push(i);
+        }
     }
 
     let geometry_struct = Geometry {
         vertices,
         dim: 3,
         indices: GeometryIndices::MultiPolygon {
-            polygon_indices: Vec::new(),
+            polygon_indices: indices,
             hole_indices: Vec::new(),
         },
     };
@@ -78,6 +89,13 @@ mod tests {
     #[test]
     fn test_convert_to_geometry() {
         let geojson_data = load_geojson(String::from("./data/sample.geojson"));
-        let geometry_result = convert_to_geometry(&geojson_data);
+        let geometry = convert_to_geometry(&geojson_data);
+
+        assert!(geometry.is_ok(), "Geometry should be ok");
+        assert_eq!(
+            geometry.unwrap().vertices.len(),
+            13,
+            "Geometry should have 13 vertices"
+        );
     }
 }
