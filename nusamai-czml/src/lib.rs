43fbf1823ac025913c2d11242e7881e3cd9d5392
nokonoko1203 <healtheworld.1203@gmail.com>
2023-11-09 11:21:14 +0900

    geometryのstructを修正

--- nusamai-czml/src/lib.rs
+++ nusamai-czml/src/lib.rs
@@ -1,22 +1,70 @@
 use serde_json::Value;
 use std::fs;
 
-enum GeometryIndices {
-    MultiPoint,
-    MultiLineString {
-        linestring_indices: Vec<Vec<u32>>,
-    },
-    MultiPolygon {
-        polygon_indices: Vec<Vec<u32>>,
-        hole_indices: Vec<Vec<u32>>,
-    },
+/// 計算や空間上の効率を優先したジオメトリの表現
+#[derive(Debug)]
+pub enum CompactGeometry {
+    MultiPoint(CompactMultiPoint),
+    MultiLineString(CompactMultiLineString),
+    MultiPolygon(CompactMultiPolygon),
 }
 
-struct Geometry {
-    pub vertices: Vec<Vec<f64>>,
+/// 計算や空間上の効率を優先した MultiPoint の表現
+#[derive(Debug)]
+pub struct CompactMultiPoint {
+    /// 頂点データ
+    pub vertices: Vec<f64>,
+
+    /// 頂点の次元 (3 or 2)
     pub dim: u8,
-    pub indices: GeometryIndices,
 }
+
+/// 計算や空間上の効率を優先した MultiString の表現
+#[derive(Debug)]
+pub struct CompactMultiLineString {
+    /// 頂点データ
+    ///
+    /// `[x0, y0, z0, x1, y1, z1, ...]`
+    pub vertices: Vec<f64>,
+
+    /// 頂点の次元 (3 or 2)
+    pub dim: u8,
+
+    /// 各 LineString の開始位置
+    ///
+    /// e.g. `[5, 12, 23]`
+    ///
+    /// 1つ目の LineString の開始位置は `0` であることが自明なので省略
+    pub part_indices: Vec<u32>,
+}
+
+/// 計算や空間上の効率を優先した MultiPolygon の表現
+#[derive(Debug)]
+pub struct CompactMultiPolygon {
+    /// 頂点データ
+    pub vertices: Vec<f64>,
+
+    /// 頂点の次元 (3 or 2)
+    pub dim: u8,
+
+    /// 各 holes (interior rings) の開始インデックス
+    /// ```
+    /// [
+    ///    6, 10, 16,
+    ///    5, 10,
+    /// ]
+    /// ```
+    pub hole_indices: Vec<u32>,
+
+    /// 各 LineString の開始位置
+    /// (hole_indices のインデックス, 頂点のインデックス)
+    ///
+    /// e.g. `[ [3, 18], [5, 32] ]`
+    ///
+    /// 1つ目の Polygon の開始位置は `[0, 0]` であることが自明なので省略
+    pub part_indices: Vec<[u32; 2]>,
+}
+
 struct CzmlBase {
     pub id: String,
     pub name: String,
@@ -28,7 +76,7 @@ fn load_geojson(filepath: String) -> String {
     data
 }
 
-fn convert_to_geometry(geojson: &str) -> Result<Geometry, serde_json::Error> {
+fn convert_to_geometry(geojson: &str) -> Result<CompactMultiPolygon, serde_json::Error> {
     let value: Value = serde_json::from_str(geojson)?;
 
     let features = &value["features"].as_array().unwrap();
@@ -66,13 +114,11 @@ fn convert_to_geometry(geojson: &str) -> Result<Geometry, serde_json::Error> {
         }
     }
 
-    let geometry_struct = Geometry {
+    let geometry_struct = CompactMultiPolygon {
         vertices,
         dim: 3,
-        indices: GeometryIndices::MultiPolygon {
-            polygon_indices: indices,
-            hole_indices: Vec::new(),
-        },
+        polygon_indices: indices,
+        hole_indices: Vec::new(),
     };
     Ok(geometry_struct)
 }
