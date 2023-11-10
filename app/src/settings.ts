2af00c1773c5d86f09c6d8311da0657cb9917dae
xinmiao1995 <kyokushinmyou@gmail.com>
2023-11-10 14:06:02 +0900

    微修正

--- app/src/settings.ts
+++ app/src/settings.ts
@@ -1,7 +1,9 @@
-export const fileTypeOptions = ['GeoJSON', 'GeoPackage', 'CZML'];
+const fileTypeOptions = ['GeoJSON', 'GeoPackage', 'CZML'];
 
-export const crsOptions = [
+const crsOptions = [
     { value: 'EPSG:6678', label: 'JGD2011 / Japan Plane Rectangular CS X' },
     { value: 'EPSG:4326', label: 'WGS 84' },
     { value: 'EPSG:3857', label: 'Web Mercator' }
 ];
+
+export { fileTypeOptions, crsOptions };
