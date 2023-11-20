ce2572ed5de604809ec6e99c6ff07a5a8c7a3c5f
Taku Fukada <naninunenor@gmail.com>
2023-11-20 15:17:57 +0900

    geometry: いくつかの改善 (#17)
    nusamai-geometry へのいくつかの改善。
    
    - 壊れたジオメトリを容易に作れないようにする #13
        - 各ジオメトリの `new()` は空のジオメトリを作るように変更
        - 代わりに生データから構築するための `from_raw`, `from_raw_unchecked` などを追加
    - #[inline] を除去: Generic型では #[inline] を付けなくてもクレート境界を超えて展開される。
    - 参考:
    https://matklad.github.io/2021/07/09/inline-in-rust.html#Inlining-in-Practice
    - 座標値のトレイト境界を変更。georust と同様に整数値も受け付けるようにする。（頂点インデクスを持たせるなどの使い方もできるようになる）
    - いくつかのテストを追加
    
    APIデザインに異論がないかを軽く見てもらえればよいと思います（実装は壊れていたら直せばいいので）。
    
    Closes #13

--- nusamai-geometry/src/compact/mod.rs
+++ nusamai-geometry/src/compact/mod.rs
@@ -4,17 +4,21 @@ mod multi_point;
 mod multi_polygon;
 mod polygon;
 
+use core::fmt::Debug;
 pub use linestring::{LineString, LineString2, LineString3};
 pub use multi_linestring::{MultiLineString, MultiLineString2, MultiLineString3};
 pub use multi_point::{MultiPoint, MultiPoint2, MultiPoint3};
 pub use multi_polygon::{MultiPolygon, MultiPolygon2, MultiPolygon3};
 pub use polygon::{Polygon, Polygon2, Polygon3};
 
-use num_traits::Float;
+use num_traits::{Num, NumCast};
+
+pub trait CoordNum: Num + Copy + NumCast + PartialOrd + Default + Debug {}
+impl<T: Num + Copy + NumCast + PartialOrd + Default + Debug> CoordNum for T {}
 
 /// Computer-friendly Geometry
 #[derive(Debug, Clone)]
-pub enum Geometry<'a, const D: usize, T: Float> {
+pub enum Geometry<'a, const D: usize, T: CoordNum> {
     LineString(MultiPolygon<'a, D, T>),
     Polygon(LineString<'a, D, T>),
     MultiPoint(MultiPoint<'a, D, T>),
@@ -24,3 +28,18 @@ pub enum Geometry<'a, const D: usize, T: Float> {
 
 pub type Geometry2<'a, T> = Geometry<'a, 2, T>;
 pub type Geometry3<'a, T> = Geometry<'a, 3, T>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_coord_num_trait() {
+        // 2D LineString with floating point numbers
+        let a: LineString<2, f32> = LineString::from_raw(vec![1.2, 2.3, 3.4, 4.5].into());
+        assert_eq!(a.len(), 2);
+        // Can also be used to store integer values (e.g., vertex indices)
+        let b: LineString<1, u32> = LineString::from_raw(vec![1, 2, 3].into());
+        assert_eq!(b.len(), 3);
+    }
+}
