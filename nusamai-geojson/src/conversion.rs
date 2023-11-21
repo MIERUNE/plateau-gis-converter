331889aa348eadb90652ad8b6b6ba62ebaacc5b9
Sorami Hisamoto <hisamoto@mierune.co.jp>
2023-11-21 15:40:00 +0900

    Add a test for polygon conversion

--- nusamai-geojson/src/conversion.rs
+++ nusamai-geojson/src/conversion.rs
@@ -53,25 +53,39 @@ fn polygon_to_rings<const D: usize, T: CoordNum>(poly: &Polygon<D, T>) -> geojso
 #[cfg(test)]
 mod tests {
     use super::*;
-    use geojson::GeoJson;
     use nusamai_geometry::{MultiPolygon2, Polygon2};
 
     #[test]
     fn test_polygon_basic() {
-        let rings: Vec<Vec<f64>> = vec![
-            vec![100., 200., 100., 100., 200., 100., 200., 200.],
-            vec![110., 190., 130., 190., 130., 110., 110., 110.],
-            vec![160., 190., 170., 110., 150., 110.],
-        ];
-        let coords: Vec<f64> = rings.into_iter().flatten().collect();
-
-        let hole_indices: Vec<u32> = vec![4];
-        let poly = Polygon2::from_raw(coords.into(), hole_indices.into());
+        let mut poly = Polygon2::new();
+        poly.add_ring([[0., 0.], [5., 0.], [5., 5.], [0., 5.]]);
+        poly.add_ring([[1., 1.], [2., 1.], [2., 2.], [1., 2.]]);
+        poly.add_ring([[3., 3.], [4., 3.], [4., 4.], [3., 4.]]);
 
         let geojson_geometry = polygon_to_geojson_geometry(&poly);
-        let geojson = GeoJson::from(geojson_geometry);
 
-        println!("{}", geojson);
+        assert!(geojson_geometry.bbox.is_none());
+        assert!(geojson_geometry.foreign_members.is_none());
+        if let geojson::Value::Polygon(rings) = geojson_geometry.value {
+            assert_eq!(rings.len(), 3);
+            assert_eq!(rings[0].len(), 5);
+            assert_eq!(rings[1].len(), 5);
+            assert_eq!(rings[2].len(), 5);
+            assert_eq!(
+                rings[0],
+                vec![[0., 0.], [5., 0.], [5., 5.], [0., 5.], [0., 0.]]
+            );
+            assert_eq!(
+                rings[1],
+                vec![[1., 1.], [2., 1.], [2., 2.], [1., 2.], [1., 1.]]
+            );
+            assert_eq!(
+                rings[2],
+                vec![[3., 3.], [4., 3.], [4., 4.], [3., 4.], [3., 3.]]
+            );
+        } else {
+            unreachable!("The result is not a GeoJSON Polygon");
+        };
     }
 
     #[test]
@@ -92,7 +106,5 @@ mod tests {
         mpoly.add_interior([[5., 1.], [6., 1.], [6., 2.], [5., 2.], [5., 1.]]);
 
         let geojson_geometry = multi_polygon_to_geojson_geometry(&mpoly);
-        println!("{}", GeoJson::from(geojson_geometry));
-        // TODO: check the result
     }
 }
