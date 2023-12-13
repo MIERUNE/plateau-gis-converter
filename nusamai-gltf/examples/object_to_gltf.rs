1788049578f5bae1434eb04d1fe8fb402d79dfc2
nokonoko1203 <healtheworld.1203@gmail.com>
2023-12-13 22:00:05 +0900

    add all_hole_indices

--- nusamai-gltf/examples/object_to_gltf.rs
+++ nusamai-gltf/examples/object_to_gltf.rs
@@ -1,6 +1,5 @@
 use citygml::{CityGMLElement, CityGMLReader, Geometries, ParseError, SubTreeReader};
 use clap::Parser;
-use nusamai_geometry::MultiPolygon;
 use nusamai_plateau::models::CityObject;
 use std::io::BufRead;
 
@@ -45,9 +44,10 @@ fn toplevel_dispatcher<R: BufRead>(
     }
 }
 
-fn extract_indices(first_obj: &TopLevelCityObject<'_>) -> (Vec<u32>, Vec<u32>) {
+fn extract_indices(first_obj: &TopLevelCityObject<'_>) -> (Vec<u32>, Vec<u32>, Vec<u32>) {
     let mut all_coords: Vec<u32> = Vec::new();
     let mut coords_spans: Vec<u32> = Vec::new();
+    let mut all_hole_indices: Vec<u32> = Vec::new();
 
     for polygon in first_obj.geometries.multipolygon.iter() {
         all_coords.extend(polygon.coords());
@@ -61,10 +61,13 @@ fn extract_indices(first_obj: &TopLevelCityObject<'_>) -> (Vec<u32>, Vec<u32>) {
                 .unwrap_or(0),
         );
         // todo: holeを考慮する必要がある
+        for hole in polygon.hole_indices() {
+            all_hole_indices.push(*hole);
+        }
     }
     // coords_spansの先頭の0を削除する
     coords_spans.remove(0);
-    (all_coords, coords_spans)
+    (all_coords, coords_spans, all_hole_indices)
 }
 
 fn main() {
@@ -96,15 +99,43 @@ fn main() {
         })
         .collect();
 
-    let first_obj = &tlc_objs[0];
+    // for (i, tlc_obj) in tlc_objs.iter().enumerate() {
+    //     let properties = &tlc_obj.cityobj;
+    //     let vertices = &tlc_obj.geometries.vertices;
+    //     let (indices, coords_spans, all_hole_indices) = extract_indices(tlc_obj);
+
+    //     if !all_hole_indices.is_empty() {
+    //         println!("i: {}", i);
+    //         println!("properties: {:?}\n", properties);
+    //         println!("vertices: {:?}\n", vertices);
+    //         println!("indices: {:?}\n", indices);
+    //         println!("coords_spans: {:?}\n", coords_spans);
+    //         println!("all_hole_indices: {:?}\n", all_hole_indices);
+    //     }
+    // }
+
+    // 17番目とかがholeを持っていた
+    let first_obj = &tlc_objs[17];
+    println!("first_obj: {:?}\n", first_obj.geometries);
 
     let properties = &first_obj.cityobj;
     let vertices = &first_obj.geometries.vertices;
-    let (indices, coords_spans) = extract_indices(first_obj);
+    let (indices, coords_spans, all_hole_indices) = extract_indices(first_obj);
 
-    // 一つのCityObjectに必要な情報
+    // 一つのCityObject(TopLevelCityObject・Feature)に必要な情報
     println!("properties: {:?}\n", properties);
     println!("vertices: {:?}\n", vertices);
     println!("indices: {:?}\n", indices);
     println!("coords_spans: {:?}\n", coords_spans);
+    println!("all_hole_indices: {:?}\n", all_hole_indices);
+
+    // todo
+    // holeも考慮する
+    // 地物の中心座標を求める
+    // 地物ごとに三角分割
+    // 頂点にIDを付与
+    // 地物ごとにバイナリバッファを作成
+    // 地物ごとにバイナリバッファをファイルに書き出し
+
+    // EXT_structural_metadata
 }
