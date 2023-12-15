ec8003913ee563163032191fa5c3256d6c0be35f
Sorami Hisamoto <hi@sorami.dev>
2023-12-15 16:45:03 +0900

    Swap lat, lon for GeoJSON

--- nusamai-geojson/src/conversion.rs
+++ nusamai-geojson/src/conversion.rs
@@ -14,11 +14,21 @@ pub fn nusamai_to_geojson_geometry<const D: usize, T: CoordNum>(
     }
 }
 
+/// Swap the order of latitude and longitude:
+/// - PLATEAU data holds the data in the order of "latitude, longitude, altitude"
+/// - GeoJSON expects the order to be "longitude, latitude, altitude"
+fn swap_lat_lon<T: CoordNum>(point: &[T]) -> Vec<T> {
+    let mut swapped = point.to_vec();
+    swapped.swap(0, 1); // Expect the length of the point to be 2 or more
+    swapped
+}
+
 fn multi_point_to_geojson_geometry<const D: usize, T: CoordNum>(
     mpoint: &nusamai_geometry::MultiPoint<D, T>,
 ) -> geojson::Geometry {
     let point_list: Vec<geojson::PointType> = mpoint
         .iter()
+        .map(swap_lat_lon)
         .map(|point| point.iter().map(|&t| t.to_f64().unwrap()).collect())
         .collect();
     geojson::Geometry::new(geojson::Value::MultiPoint(point_list))
@@ -29,6 +39,7 @@ fn linestring_to_geojson_geometry<const D: usize, T: CoordNum>(
 ) -> geojson::Geometry {
     let point_list: geojson::LineStringType = linestring
         .iter()
+        .map(swap_lat_lon)
         .map(|point| point.iter().map(|&t| t.to_f64().unwrap()).collect())
         .collect();
     geojson::Geometry::new(geojson::Value::LineString(point_list))
@@ -42,6 +53,7 @@ fn multi_linestring_to_geojson_geometry<const D: usize, T: CoordNum>(
         .map(|linestring| {
             linestring
                 .iter()
+                .map(swap_lat_lon)
                 .map(|point| point.iter().map(|&t| t.to_f64().unwrap()).collect())
                 .collect()
         })
@@ -70,6 +82,7 @@ fn polygon_to_rings<const D: usize, T: CoordNum>(poly: &Polygon<D, T>) -> geojso
         .map(|linestring| {
             linestring
                 .iter_closed()
+                .map(swap_lat_lon)
                 .map(|slice| slice.iter().map(|&t| t.to_f64().unwrap()).collect())
                 .collect()
         })
@@ -86,8 +99,8 @@ mod tests {
     fn test_multi_point_basic() {
         let mut mpoint = MultiPoint2::new();
         mpoint.push(&[0., 0.]);
-        mpoint.push(&[1., 1.]);
-        mpoint.push(&[2., 2.]);
+        mpoint.push(&[1., 2.]);
+        mpoint.push(&[3., 4.]);
 
         let geojson_geometry = multi_point_to_geojson_geometry(&mpoint);
 
@@ -97,8 +110,8 @@ mod tests {
         if let geojson::Value::MultiPoint(points) = geojson_geometry.value {
             assert_eq!(points.len(), mpoint.len());
             assert_eq!(points[0], vec![0., 0.]);
-            assert_eq!(points[1], vec![1., 1.]);
-            assert_eq!(points[2], vec![2., 2.]);
+            assert_eq!(points[1], vec![2., 1.]); // lon, lat swapped
+            assert_eq!(points[2], vec![4., 3.]);
         } else {
             unreachable!("The result is not a GeoJSON MultiPoint");
         };
@@ -108,8 +121,8 @@ mod tests {
     fn test_linestring_basic() {
         let mut linestring = nusamai_geometry::LineString2::new();
         linestring.push(&[0., 0.]);
-        linestring.push(&[1., 1.]);
-        linestring.push(&[2., 2.]);
+        linestring.push(&[1., 2.]);
+        linestring.push(&[3., 4.]);
 
         let geojson_geometry = linestring_to_geojson_geometry(&linestring);
 
@@ -119,8 +132,8 @@ mod tests {
         if let geojson::Value::LineString(points) = geojson_geometry.value {
             assert_eq!(points.len(), linestring.len());
             assert_eq!(points[0], vec![0., 0.]);
-            assert_eq!(points[1], vec![1., 1.]);
-            assert_eq!(points[2], vec![2., 2.]);
+            assert_eq!(points[1], vec![2., 1.]);
+            assert_eq!(points[2], vec![4., 3.]);
         } else {
             unreachable!("The result is not a GeoJSON LineString");
         };
@@ -129,9 +142,9 @@ mod tests {
     #[test]
     fn test_multi_linestring_basic() {
         let mut mls = MultiLineString2::new();
-        mls.add_linestring(vec![[0., 0.], [1., 1.]]);
-        mls.add_linestring(vec![[2., 2.], [3., 3.]]);
-        mls.add_linestring(vec![[4., 4.], [5., 5.], [6., 6.]]);
+        mls.add_linestring(vec![[0., 0.], [1., 2.]]);
+        mls.add_linestring(vec![[3., 4.], [5., 6.]]);
+        mls.add_linestring(vec![[7., 8.], [9., 10.], [11., 12.]]);
 
         let geojson_geometry = multi_linestring_to_geojson_geometry(&mls);
 
@@ -140,9 +153,9 @@ mod tests {
 
         if let geojson::Value::MultiLineString(lines) = geojson_geometry.value {
             assert_eq!(lines.len(), mls.len());
-            assert_eq!(lines[0], vec![[0., 0.], [1., 1.]]);
-            assert_eq!(lines[1], vec![[2., 2.], [3., 3.]]);
-            assert_eq!(lines[2], vec![[4., 4.], [5., 5.], [6., 6.]]);
+            assert_eq!(lines[0], vec![[0., 0.], [2., 1.]]);
+            assert_eq!(lines[1], vec![[4., 3.], [6., 5.]]);
+            assert_eq!(lines[2], vec![[8., 7.], [10., 9.], [12., 11.]]);
         } else {
             unreachable!("The result is not a GeoJSON MultiLineString");
         };
@@ -151,9 +164,9 @@ mod tests {
     #[test]
     fn test_polygon_basic() {
         let mut poly = Polygon2::new();
-        poly.add_ring([[0., 0.], [5., 0.], [5., 5.], [0., 5.]]);
-        poly.add_ring([[1., 1.], [2., 1.], [2., 2.], [1., 2.]]);
-        poly.add_ring([[3., 3.], [4., 3.], [4., 4.], [3., 4.]]);
+        poly.add_ring([[0., 0.], [51., 0.], [50., 50.], [0., 52.]]);
+        poly.add_ring([[10., 10.], [21., 10.], [20., 20.], [10., 22.]]);
+        poly.add_ring([[30., 30.], [41., 30.], [40., 40.], [30., 42.]]);
 
         let geojson_geometry = polygon_to_geojson_geometry(&poly);
 
@@ -166,15 +179,15 @@ mod tests {
             assert_eq!(rings[2].len(), 5);
             assert_eq!(
                 rings[0],
-                vec![[0., 0.], [5., 0.], [5., 5.], [0., 5.], [0., 0.]]
+                vec![[0., 0.], [0., 51.], [50., 50.], [52., 0.], [0., 0.]] // lon, lat swapped
             );
             assert_eq!(
                 rings[1],
-                vec![[1., 1.], [2., 1.], [2., 2.], [1., 2.], [1., 1.]]
+                vec![[10., 10.], [10., 21.], [20., 20.], [22., 10.], [10., 10.]]
             );
             assert_eq!(
                 rings[2],
-                vec![[3., 3.], [4., 3.], [4., 4.], [3., 4.], [3., 3.]]
+                vec![[30., 30.], [30., 41.], [40., 40.], [42., 30.], [30., 30.]]
             );
         } else {
             unreachable!("The result is not a GeoJSON Polygon");
@@ -184,9 +197,24 @@ mod tests {
     #[test]
     fn test_polygon_basic_3d() {
         let mut poly = Polygon3::new();
-        poly.add_ring([[0., 0., 99.], [5., 0., 99.], [5., 5., 99.], [0., 5., 99.]]);
-        poly.add_ring([[1., 1., 99.], [2., 1., 99.], [2., 2., 99.], [1., 2., 99.]]);
-        poly.add_ring([[3., 3., 99.], [4., 3., 99.], [4., 4., 99.], [3., 4., 99.]]);
+        poly.add_ring([
+            [0., 0., 99.],
+            [51., 0., 99.],
+            [50., 50., 99.],
+            [0., 52., 99.],
+        ]);
+        poly.add_ring([
+            [10., 10., 99.],
+            [21., 10., 99.],
+            [20., 20., 99.],
+            [10., 22., 99.],
+        ]);
+        poly.add_ring([
+            [30., 30., 99.],
+            [41., 30., 99.],
+            [40., 40., 99.],
+            [30., 42., 99.],
+        ]);
 
         let geojson_geometry = polygon_to_geojson_geometry(&poly);
 
@@ -201,30 +229,30 @@ mod tests {
                 rings[0],
                 vec![
                     [0., 0., 99.],
-                    [5., 0., 99.],
-                    [5., 5., 99.],
-                    [0., 5., 99.],
+                    [0., 51., 99.],
+                    [50., 50., 99.],
+                    [52., 0., 99.],
                     [0., 0., 99.]
                 ]
             );
             assert_eq!(
                 rings[1],
                 vec![
-                    [1., 1., 99.],
-                    [2., 1., 99.],
-                    [2., 2., 99.],
-                    [1., 2., 99.],
-                    [1., 1., 99.]
+                    [10., 10., 99.],
+                    [10., 21., 99.],
+                    [20., 20., 99.],
+                    [22., 10., 99.],
+                    [10., 10., 99.]
                 ]
             );
             assert_eq!(
                 rings[2],
                 vec![
-                    [3., 3., 99.],
-                    [4., 3., 99.],
-                    [4., 4., 99.],
-                    [3., 4., 99.],
-                    [3., 3., 99.]
+                    [30., 30., 99.],
+                    [30., 41., 99.],
+                    [40., 40., 99.],
+                    [42., 30., 99.],
+                    [30., 30., 99.]
                 ]
             );
         } else {
@@ -263,15 +291,15 @@ mod tests {
                         assert_eq!(rings[2].len(), 5);
                         assert_eq!(
                             rings[0],
-                            vec![[0., 0.], [5., 0.], [5., 5.], [0., 5.], [0., 0.]]
+                            vec![[0., 0.], [0., 5.], [5., 5.], [5., 0.], [0., 0.]] // lon, lat swapped
                         );
                         assert_eq!(
                             rings[1],
-                            vec![[1., 1.], [2., 1.], [2., 2.], [1., 2.], [1., 1.]]
+                            vec![[1., 1.], [1., 2.], [2., 2.], [2., 1.], [1., 1.]]
                         );
                         assert_eq!(
                             rings[2],
-                            vec![[3., 3.], [4., 3.], [4., 4.], [3., 4.], [3., 3.]]
+                            vec![[3., 3.], [3., 4.], [4., 4.], [4., 3.], [3., 3.]]
                         );
                     }
                     1 => {
@@ -280,11 +308,11 @@ mod tests {
                         assert_eq!(rings[1].len(), 5);
                         assert_eq!(
                             rings[0],
-                            vec![[4., 0.], [7., 0.], [7., 3.], [4., 3.], [4., 0.]]
+                            vec![[0., 4.], [0., 7.], [3., 7.], [3., 4.], [0., 4.]]
                         );
                         assert_eq!(
                             rings[1],
-                            vec![[5., 1.], [6., 1.], [6., 2.], [5., 2.], [5., 1.]]
+                            vec![[1., 5.], [1., 6.], [2., 6.], [2., 5.], [1., 5.]]
                         );
                     }
                     2 => {
@@ -293,11 +321,11 @@ mod tests {
                         assert_eq!(rings[1].len(), 5);
                         assert_eq!(
                             rings[0],
-                            vec![[4., 0.], [7., 0.], [7., 3.], [4., 3.], [4., 0.]]
+                            vec![[0., 4.], [0., 7.], [3., 7.], [3., 4.], [0., 4.]]
                         );
                         assert_eq!(
                             rings[1],
-                            vec![[5., 1.], [6., 1.], [6., 2.], [5., 2.], [5., 1.]]
+                            vec![[1., 5.], [1., 6.], [2., 6.], [2., 5.], [1., 5.]]
                         );
                     }
                     _ => unreachable!(),
