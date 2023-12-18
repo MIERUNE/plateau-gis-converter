64a00f2d8dc0f71cd9b30ba8fd6be2c255ea5f48
Sorami Hisamoto <hi@sorami.dev>
2023-12-18 15:28:15 +0900

    If multipolygon or multilinestring empty, skip

--- nusamai-geojson/src/lib.rs
+++ nusamai-geojson/src/lib.rs
@@ -12,30 +12,38 @@ pub struct TopLevelCityObject<'a> {
 pub fn toplevel_city_object_to_geojson_features(
     tlco: &TopLevelCityObject,
 ) -> Vec<geojson::Feature> {
-    let mpoly_geojson_geom = conversion::multipolygon_to_geojson_geometry(
-        &tlco.geometries.vertices,
-        &tlco.geometries.multipolygon,
-    );
-    let mpoly_geojson_feat = geojson::Feature {
-        bbox: None,
-        geometry: Some(mpoly_geojson_geom),
-        id: None,
-        properties: None, // TODO: from `tlco.cityobj`
-        foreign_members: None,
-    };
+    let mut geojson_features: Vec<geojson::Feature> = vec![];
 
-    let mls_geojson_geom = conversion::multilinestring_to_geojson_geometry(
-        &tlco.geometries.vertices,
-        &tlco.geometries.multilinestring,
-    );
-    // TODO: Handle properties (`tlco.cityobj` -> `geojson::Feature.properties`)
-    let mls_geojson_feat = geojson::Feature {
-        bbox: None,
-        geometry: Some(mls_geojson_geom),
-        id: None,
-        properties: None, // TODO: from `tlco.cityobj`
-        foreign_members: None,
-    };
+    if !tlco.geometries.multipolygon.is_empty() {
+        let mpoly_geojson_geom = conversion::multipolygon_to_geojson_geometry(
+            &tlco.geometries.vertices,
+            &tlco.geometries.multipolygon,
+        );
+        let mpoly_geojson_feat = geojson::Feature {
+            bbox: None,
+            geometry: Some(mpoly_geojson_geom),
+            id: None,
+            properties: None, // TODO: from `tlco.cityobj`
+            foreign_members: None,
+        };
+        geojson_features.push(mpoly_geojson_feat);
+    }
 
-    vec![mpoly_geojson_feat, mls_geojson_feat]
+    if !tlco.geometries.multilinestring.is_empty() {
+        let mls_geojson_geom: geojson::Geometry = conversion::multilinestring_to_geojson_geometry(
+            &tlco.geometries.vertices,
+            &tlco.geometries.multilinestring,
+        );
+        // TODO: Handle properties (`tlco.cityobj` -> `geojson::Feature.properties`)
+        let mls_geojson_feat = geojson::Feature {
+            bbox: None,
+            geometry: Some(mls_geojson_geom),
+            id: None,
+            properties: None, // TODO: from `tlco.cityobj`
+            foreign_members: None,
+        };
+        geojson_features.push(mls_geojson_feat);
+    }
+
+    geojson_features
 }
