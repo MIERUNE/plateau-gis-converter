d8e920f9f1d75b1b25d842c6a8ffe8041ead1406
Sorami Hisamoto <hi@sorami.dev>
2023-12-25 14:40:07 +0900

    CityGMLをパースする部分を追加

--- nusamai-gpkg/examples/gml2gpkg.rs
+++ nusamai-gpkg/examples/gml2gpkg.rs
@@ -1,8 +1,64 @@
+use citygml::{CityGMLElement, CityGMLReader, ParseError, SubTreeReader};
+use clap::Parser;
+use nusamai_plateau::TopLevelCityObject;
 use sqlx::Row;
+use std::io::BufRead;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(required = true)]
+    filename: String,
+}
+
+fn toplevel_dispatcher<R: BufRead>(
+    st: &mut SubTreeReader<R>,
+) -> Result<Vec<TopLevelCityObject>, ParseError> {
+    let mut cityobjs: Vec<TopLevelCityObject> = vec![];
+
+    match st.parse_children(|st| match st.current_path() {
+        b"core:cityObjectMember" => {
+            let mut cityobj: nusamai_plateau::models::TopLevelCityObject = Default::default();
+            cityobj.parse(st)?;
+            let geometries = st.collect_geometries();
+
+            if let Some(root) = cityobj.into_object() {
+                let obj = TopLevelCityObject { root, geometries };
+                cityobjs.push(obj);
+            }
+
+            Ok(())
+        }
+        b"gml:boundedBy" | b"app:appearanceMember" => {
+            st.skip_current_element()?;
+            Ok(())
+        }
+        other => Err(ParseError::SchemaViolation(format!(
+            "Unrecognized element {}",
+            String::from_utf8_lossy(other)
+        ))),
+    }) {
+        Ok(_) => Ok(cityobjs),
+        Err(e) => {
+            println!("Err: {:?}", e);
+            Err(e)
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // TODO: Handle output path
+    let args = Args::parse();
+
+    let reader = std::io::BufReader::new(std::fs::File::open(args.filename).unwrap());
+    let mut xml_reader = quick_xml::NsReader::from_reader(reader);
+    let _cityobjs = match CityGMLReader::new().start_root(&mut xml_reader) {
+        Ok(mut st) => match toplevel_dispatcher(&mut st) {
+            Ok(items) => items,
+            Err(e) => panic!("Err: {:?}", e),
+        },
+        Err(e) => panic!("Err: {:?}", e),
+    };
+
     let output_path = "output.gpkg";
     let db_url: &str = &format!("sqlite://{}", output_path);
 
