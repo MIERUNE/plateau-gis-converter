f0eca6744095c9a1b952cd159bded5687af2fcbd
nokonoko1203 <healtheworld.1203@gmail.com>
2023-12-27 14:52:43 +0900

    provisional implementation

--- nusamai-plateau/src/lib.rs
+++ nusamai-plateau/src/lib.rs
@@ -7,3 +7,12 @@ pub struct TopLevelCityObject {
     pub root: citygml::object::ObjectValue,
     pub geometries: citygml::geometry::Geometries,
 }
+
+// 仮実装
+
+impl TopLevelCityObject {
+    pub fn to_feature(&self) {}
+}
+
+#[derive(Debug, Clone, Default)]
+struct Feature {}
