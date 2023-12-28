ae624551663e66a007cc11efe0b6d4197fa6a183
Sorami Hisamoto <hisamoto@mierune.co.jp>
2023-12-28 13:13:28 +0900

    とりあえずasyncをブロックして実行

--- nusamai/src/sink/gpkg/mod.rs
+++ nusamai/src/sink/gpkg/mod.rs
@@ -1,17 +1,13 @@
 //! GeoPackage sink
 
-use std::fs::File;
-use std::io::{BufWriter, Write};
-
 use rayon::prelude::*;
+use tokio;
 
 use crate::configuration::Config;
 use crate::pipeline::{Feedback, Receiver};
 use crate::sink::{DataSink, DataSinkProvider, SinkInfo};
 
 use nusamai_gpkg::GpkgHandler;
-use nusamai_plateau::TopLevelCityObject;
-use serde_json::Value;
 
 pub struct GpkgSinkProvider {}
 
@@ -36,50 +32,39 @@ pub struct GpkgSink {
     n_features: usize,
 }
 
+async fn async_function() -> Result<(), ()> {
+    Ok(())
+}
+
 impl DataSink for GpkgSink {
     fn run(&mut self, upstream: Receiver, feedback: &mut Feedback) {
-        //     let (sender, receiver) = std::sync::mpsc::sync_channel(100);
+        let (sender, receiver) = std::sync::mpsc::sync_channel(100);
 
-        //     rayon::join(
-        //         || {
-        //             // Convert TopLevelCityObjects to GeoJSON objects
+        let runtime = tokio::runtime::Runtime::new().unwrap();
+        let handler = runtime.block_on(GpkgHandler::init("output.gpkg")).unwrap();
 
-        //             let output_path = "output.gpkg";
-        //             // let handler = nusamai_gpkg::GpkgHandler::init(output_path).await.unwrap();
+        let _ = upstream
+            .into_iter()
+            .par_bridge()
+            .try_for_each_with(sender, |sender, parcel| {
+                if feedback.is_cancelled() {
+                    return Err(());
+                }
 
-        //             let _ = upstream.into_iter().par_bridge().try_for_each_with(
-        //                 sender,
-        //                 |sender, parcel| {
-        //                     if feedback.is_cancelled() {
-        //                         return Err(());
-        //                     }
+                if !parcel.cityobj.geometries.multipolygon.is_empty() {
+                    runtime.block_on(handler.add_multi_polygon_feature(
+                        &parcel.cityobj.geometries.vertices,
+                        &parcel.cityobj.geometries.multipolygon,
+                    ));
+                }
 
-        //                     // TODO
-        //                     //
-        //                     // let geometries = &parcel.cityobj.geometries;
-        //                     //
-        //                     // if !geometries.multipolygon.is_empty() {
-        //                     //     handler
-        //                     //         .add_multi_polygon_feature(
-        //                     //             &geometries.vertices,
-        //                     //             &geometries.multipolygon,
-        //                     //         )
-        //                     //         .await;
-        //                     // }
+                if sender.send(parcel.cityobj).is_err() {
+                    println!("sink cancelled");
+                    return Err(());
+                };
 
-        //                     // if sender.send(geometries).is_err() {
-        //                     //     println!("sink cancelled");
-        //                     //     return Err(());
-        //                     // };
-
-        //                     Ok(())
-        //                 },
-        //             );
-        //         },
-        //         || {
-        //             receiver;
-        //         },
-        //     );
+                Ok(())
+            });
     }
 }
 
