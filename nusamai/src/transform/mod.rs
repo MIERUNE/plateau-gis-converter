afe8c185cae2f4f557f2956541c5b2e83b0476f8
Taku Fukada <naninunenor@gmail.com>
2024-01-06 22:01:07 +0900

    swap xy

--- nusamai/src/transform/mod.rs
+++ nusamai/src/transform/mod.rs
@@ -5,11 +5,15 @@ pub struct NoopTransformer {}
 impl Transformer for NoopTransformer {
     fn transform(
         &self,
-        parcel: Parcel,
+        mut parcel: Parcel,
         downstream: &Sender,
         feedback: &Feedback,
     ) -> Result<(), TransformError> {
-        // no-op
+        // swap x and y
+        parcel.cityobj.geometries.vertices.iter_mut().for_each(|v| {
+            (v[0], v[1], v[2]) = (v[1], v[0], v[2]);
+        });
+
         if downstream.send(parcel).is_err() {
             feedback.cancel();
         };
