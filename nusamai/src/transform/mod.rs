ab05cf2c6e4e3f63c913a076b72f9a185affc494
Taku Fukada <naninunenor@gmail.com>
2024-01-07 16:43:34 +0900

    JGD2011→WGS84

--- nusamai/src/transform/mod.rs
+++ nusamai/src/transform/mod.rs
@@ -1,17 +1,32 @@
 use crate::pipeline::{Feedback, Parcel, Sender, TransformError, Transformer};
+use nusamai_projection::vshift::JGD2011ToWGS84;
 
-pub struct NoopTransformer {}
+pub struct DummyTransformer {
+    jgd2wgs: JGD2011ToWGS84,
+}
 
-impl Transformer for NoopTransformer {
+impl Default for DummyTransformer {
+    fn default() -> Self {
+        Self {
+            jgd2wgs: JGD2011ToWGS84::from_embedded_model(),
+        }
+    }
+}
+
+impl Transformer for DummyTransformer {
     fn transform(
         &self,
         mut parcel: Parcel,
         downstream: &Sender,
         feedback: &Feedback,
     ) -> Result<(), TransformError> {
-        // swap x and y
+        // 仮実装
         parcel.cityobj.geometries.vertices.iter_mut().for_each(|v| {
-            (v[0], v[1], v[2]) = (v[1], v[0], v[2]);
+            // Swap x and y (lat, lng -> lng, lat)
+            let (lng, lat, height) = (v[1], v[0], v[2]);
+
+            // JGD2011 to WGS 84 (elevation to ellipsoidal height)
+            (v[0], v[1], v[2]) = self.jgd2wgs.convert(lng, lat, height);
         });
 
         if downstream.send(parcel).is_err() {
