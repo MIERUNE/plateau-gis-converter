65b493b698378b43f75161665d6fac1491fb058e
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2024-01-10 09:58:08 +0800

    landuse model

---
+++ nusamai-plateau/src/models/iur/uro/landuse.rs
@@ -0,0 +1,80 @@
+use nusamai_citygml::{
+    citygml_data, CityGMLElement, Code, GYear, Measure,
+};
+
+#[citygml_data(name = "uro:LandUseDetailAttribute")]
+pub struct LandUseDetailAttribute {
+    #[citygml(path = b"uro:id")]
+    pub id: Option<String>,
+
+    #[citygml(path = b"uro:orgLandUse")]
+    pub org_land_use: Option<Code>,
+
+    #[citygml(path = b"uro:nominalArea")]
+    pub nominal_area: Option<Measure>,
+
+    #[citygml(path = b"uro:ownerType")]
+    pub owner_type: Option<Code>,
+
+    #[citygml(path = b"uro:owner")]
+    pub owner: Option<String>,
+
+    #[citygml(path = b"uro:areaInSquareMeter")]
+    pub area_in_square_meter: Option<Measure>,
+
+    #[citygml(path = b"uro:areaInHa")]
+    pub area_in_ha: Option<Measure>,
+
+    #[citygml(path = b"uro:buildingCoverageRate")]
+    pub building_coverage_rate: Option<f64>,
+
+    #[citygml(path = b"uro:floorAreaRate")]
+    pub floor_area_rate: Option<f64>,
+
+    #[citygml(path = b"uro:specifiedBuildingCoverageRate")]
+    pub specified_building_coverage_rate: Option<f64>,
+
+    #[citygml(path = b"uro:specifiedFloorAreaRate")]
+    pub specified_floor_area_rate: Option<f64>,
+
+    #[citygml(path = b"uro:standardFloorAreaRate")]
+    pub standard_floor_area_rate: Option<f64>,
+
+    #[citygml(path = b"uro:urbanPlanType")]
+    pub urban_plan_type: Option<Code>,
+
+    #[citygml(path = b"uro:areaClassificationType")]
+    pub area_classification_type: Option<Code>,
+
+    #[citygml(path = b"uro:districtsAndZonesType")]
+    pub districts_and_zones_type: Vec<Code>,
+
+    #[citygml(path = b"uro:prefecture")]
+    pub prefecture: Option<Code>,
+
+    #[citygml(path = b"uro:city")]
+    pub city: Option<Code>,
+
+    #[citygml(path = b"uro:reference")]
+    pub reference: Option<String>,
+
+    #[citygml(path = b"uro:note")]
+    pub note: Option<String>,
+
+    #[citygml(path = b"uro:surveyYear")]
+    pub survey_year: Option<GYear>,
+
+}
+
+#[citygml_data(name = "uro:LandUseDataQualityAttribute")]
+pub struct LandUseDataQualityAttribute {
+    #[citygml(path = b"uro:srcScale")]
+    pub src_scale: Vec<Code>,
+
+    #[citygml(path = b"uro:geometrySrcDesc")]
+    pub geometry_src_desc: Vec<Code>,
+
+    #[citygml(path = b"uro:thematicSrcDesc")]
+    pub thematic_src_desc: Vec<Code>,
+
+}
