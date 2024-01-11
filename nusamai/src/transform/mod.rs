170ec211e4fa8b6a82fb15a84ad905c9d1b03829
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-11 17:46:16 +0900

    extract only features

--- nusamai/src/transform/mod.rs
+++ nusamai/src/transform/mod.rs
@@ -3,7 +3,7 @@ use indexmap::IndexMap;
 
 use crate::pipeline::{Feedback, Parcel, Sender, TransformError, Transformer};
 
-use nusamai_citygml::object::{CityObject, Feature, Map};
+use nusamai_citygml::object::{CityObject, Data, Feature, Map};
 use nusamai_citygml::Value;
 use nusamai_projection::crs::*;
 use nusamai_projection::vshift::JGD2011ToWGS84;
@@ -78,17 +78,47 @@ pub struct ObjectController {
 }
 
 impl ObjectController {
+    fn extract_features(&self, value: Value) -> Vec<Feature> {
+        match value {
+            Value::Array(vec) => vec
+                .iter()
+                .flat_map(|v| self.extract_features(v.clone()))
+                .collect(),
+            Value::Feature(feature) => {
+                vec![feature.clone()]
+            }
+            _ => Vec::new(),
+        }
+    }
+
+    fn attribute_parser(&self, feature: &Feature, gml_id: String) -> Vec<Feature> {
+        let mut features: Vec<Feature> = Vec::new();
+
+        for (key, value) in feature.attributes.iter() {
+            let f = self.extract_features(value.clone());
+            features.extend(f);
+        }
+
+        println!("{:?}", features);
+        features
+    }
+
     pub fn to_tabular(&self, cityobj: &CityObject) -> Vec<Layer> {
         let manager = LayerManager { layers: None };
 
-        let feature: &Feature = match &cityobj.root {
+        let toplevel_feature: &Feature = match &cityobj.root {
             Value::Feature(f) => f,
             _ => todo!(),
         };
+        let parent_gml_id = &toplevel_feature.id;
+        println!("{:?}", parent_gml_id);
 
-        let features = &self.parse_feature(feature);
-        println!("{:?}", features);
+        let features = &self.attribute_parser(toplevel_feature, parent_gml_id.clone().unwrap());
 
+        // テスト用の出力
+        let file = std::fs::File::create("/Users/satoru/Downloads/output/test.json").unwrap();
+        let writer = std::io::BufWriter::new(file);
+        serde_json::to_writer_pretty(writer, &cityobj.root).unwrap();
         todo!("CityObject to Vec<Layer>")
     }
 
