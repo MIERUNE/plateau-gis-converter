717544914e113216857e49ef65af95eab2bee5ad
Sorami Hisamoto <hisamoto@mierune.co.jp>
2024-01-11 16:18:48 +0900

    log

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,15 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+use std::sync::{Arc, Mutex};
+
 use nusamai::pipeline::Canceller;
 use nusamai::sink::geojson::GeoJsonSinkProvider;
 use nusamai::sink::DataSinkProvider;
 use nusamai::source::citygml::CityGMLSourceProvider;
 use nusamai::source::{DataSource, DataSourceProvider};
 use nusamai::transform::DummyTransformer;
-use std::sync::{Arc, Mutex};
 
 fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info")
+    }
+    pretty_env_logger::init();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![run])
         .run(tauri::generate_context!())
@@ -30,7 +37,7 @@ fn run(input_path: String, output_path: String, filetype: String) {
             Box::new(CityGMLSourceProvider { filenames });
         let mut source_params = source_provider.parameters();
         if let Err(err) = source_params.validate() {
-            eprintln!("Error validating source parameters: {:?}", err);
+            log::error!("Error validating source parameters: {:?}", err);
             return;
         }
         source_provider.create(&source_params)
@@ -40,11 +47,11 @@ fn run(input_path: String, output_path: String, filetype: String) {
         let sink_provider = Box::new(GeoJsonSinkProvider {});
         let mut sink_params = sink_provider.parameters();
         if let Err(err) = sink_params.update_values_with_str(&sinkopt) {
-            eprintln!("Error parsing sink options: {:?}", err);
+            log::error!("Error parsing sink options: {:?}", err);
             return;
         };
         if let Err(err) = sink_params.validate() {
-            eprintln!("Error validating source parameters: {:?}", err);
+            log::error!("Error validating source parameters: {:?}", err);
             return;
         }
         sink_provider.create(&sink_params)
@@ -60,7 +67,7 @@ fn run(input_path: String, output_path: String, filetype: String) {
         // log watcher
         scope.spawn(move || {
             for msg in watcher {
-                println!("Feedback message from the pipeline {:?}", msg);
+                log::info!("Feedback message from the pipeline {:?}", msg);
             }
         });
     });
@@ -68,6 +75,6 @@ fn run(input_path: String, output_path: String, filetype: String) {
     // wait for the pipeline to finish
     handle.join();
     if canceller.lock().unwrap().is_cancelled() {
-        println!("Pipeline cancelled");
+        log::info!("Pipeline cancelled");
     }
 }
