be698cec0389a6f40d39d4657c31b2758ff93852
Sorami Hisamoto <hisamoto@mierune.co.jp>
2024-01-12 16:42:35 +0900

    wip

--- app/src/routes/InputSelector.svelte
+++ app/src/routes/InputSelector.svelte
@@ -6,7 +6,7 @@
 
 	async function openInputDialog() {
 		const res = await dialog.open({
-			multiple: false,
+			multiple: true,
 			directory: false,
 			filters: [
 				{
@@ -18,7 +18,7 @@
 		inputPath = Array.isArray(res) ? res[0] : res ?? '';
 	}
 
-	let isFolderMode = false;
+	let isFolderMode = true;
 </script>
 
 <div class="">
@@ -28,21 +28,29 @@
 			<button
 				type="button"
 				class="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-				class:active={!isFolderMode}
-				on:click={() => (isFolderMode = false)}
-				><Icon icon="material-symbols:folder" />ファイルを選択</button
+				class:active={isFolderMode}
+				on:click={() => (isFolderMode = true)}
+				><Icon icon="material-symbols:folder" />フォルダ選択</button
 			>
 			<button
 				type="button"
 				class="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-				class:active={isFolderMode}
-				on:click={() => (isFolderMode = true)}><Icon icon="ph:files" />フォルダを選択</button
+				class:active={!isFolderMode}
+				on:click={() => (isFolderMode = false)}><Icon icon="ph:files" />ファイル選択</button
 			>
 		</span>
 	</div>
 
 	<div class="rounded-xl flex flex-col gap-4 justify-center items-center">
 		{#if isFolderMode}
+			<div>
+				<button
+					on:click={openInputDialog}
+					class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-lg px-2 py-2 shadow"
+					>フォルダ選択</button
+				>
+			</div>
+		{:else}
 			<div>
 				<button
 					on:click={openInputDialog}
@@ -55,14 +63,6 @@
 					{inputPath.length}ファイルを選択中
 				</div>
 				<Icon icon="material-symbols:format-list-bulleted" />
-			</div>
-		{:else}
-			<div>
-				<button
-					on:click={openInputDialog}
-					class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-lg px-2 py-2 shadow"
-					>フォルダ選択</button
-				>
 			</div>
 		{/if}
 	</div>
