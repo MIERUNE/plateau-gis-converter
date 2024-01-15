5fd1734ac076fa729f2e9cc7fa083a4070501286
Taku Fukada <naninunenor@gmail.com>
2024-01-15 13:49:21 +0900

    update

--- nusamai-citygml/src/schema.rs
+++ nusamai-citygml/src/schema.rs
@@ -1,36 +1,24 @@
 use std::collections::HashMap;
 
 pub struct Schema {
-    pub types: HashMap<String, StereoType>,
+    pub types: HashMap<String, TypeDef>,
 }
 
-pub enum StereoType {
-    DataType(DataTypeDef),
-    FeatureType(FeatureTypeDef),
-    BasicType(BasicTypeDef),
-}
-
-pub struct Attribute {
+pub struct TypeDef {
     pub name: String,
-    pub ty: String,
+    pub ty: Type,
     pub min_occurs: Option<u16>,
     pub max_occurs: Option<u16>,
 }
 
-pub struct DataTypeDef {
-    /// name to type
-    pub attributes: Vec<Attribute>,
+pub enum Type {
+    BasicType(BasicType),
+    ComplexType(ComplexType),
 }
 
-pub struct FeatureTypeDef {
-    /// name to type
-    pub attributes: Vec<Attribute>,
-}
-
-pub struct BasicTypeDef {
-    pub ty: BasicType,
-    /// scalar (false) or array (true)
-    pub multiple: bool,
+pub struct ComplexType {
+    /// name to type mapping
+    pub attributes: Vec<TypeDef>,
 }
 
 pub enum BasicType {
