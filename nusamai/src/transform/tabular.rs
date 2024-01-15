44583faec59fd907668c1ba53d8f45009fcd332a
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-15 15:14:44 +0900

    extract attribute separately

--- nusamai/src/transform/tabular.rs
+++ nusamai/src/transform/tabular.rs
@@ -1,7 +1,6 @@
-use ahash::random_state::RandomState;
 use indexmap::IndexMap;
 
-use nusamai_citygml::object::{CityObject, Feature, Map};
+use nusamai_citygml::object::{CityObject, Data, Feature};
 use nusamai_citygml::Value;
 
 // 以下、仮実装
@@ -47,26 +46,84 @@ impl ObjectSeparator {
         }
     }
 
-    fn attribute_parser(&self, feature: &Feature, gml_id: String) -> Vec<Feature> {
-        let mut features: Vec<Feature> = Vec::new();
-
-        for (key, value) in feature.attributes.iter() {
-            let f = self.extract_features(value.clone());
-            features.extend(f);
+    fn extract_data(&self, value: Value) -> Vec<Data> {
+        match value {
+            Value::Array(vec) => vec
+                .iter()
+                .flat_map(|v: &Value| self.extract_data(v.clone()))
+                .collect(),
+            Value::Data(data) => {
+                vec![data.clone()]
+            }
+            _ => Vec::new(),
         }
+    }
+
+    fn attribute_parser(&self, feature: &Feature, gml_id: String) -> Vec<Feature> {
+        let mut primitive_attributes: IndexMap<String, Value> = IndexMap::new();
+        let mut features: Vec<Feature> = Vec::new();
+        let mut data_list: Vec<Data> = Vec::new();
+        let mut array_list: Vec<Vec<Value>> = Vec::new();
+
+        // フラット化されたdataとfeatureを取得
+        // それ以外の情報も取得
+        for (key, value) in feature.attributes.iter() {
+            match value {
+                Value::Array(a) => {
+                    array_list.push(a.clone());
+                }
+                Value::Data(d) => {
+                    data_list.push(d.clone());
+                }
+                Value::Feature(f) => {
+                    features.push(f.clone());
+                }
+                _ => {
+                    primitive_attributes.insert(key.clone(), value.clone());
+                }
+            }
+
+            // let d = self.extract_data(value.clone());
+            // data_list.extend(d);
+
+            // let f = self.extract_features(value.clone());
+            // features.extend(f);
+        }
+
+        // オプションなし→全ての最小のLODを取得し、一部の属性はテーブルを分割
+        // ★ルール
+        // ・最も単純なLODのみが読み込まれる
+        // トップレベル.geometriesの中から最小LODのもの？
+        // と思ったが、QGISプラグインはLOD0と1が読み込まれたら1が表示されていた
+        // これがなければ、個別に読み取りに行く？
+        // ・Dataは別テーブルに分割
+        // Dataはattributesを展開して、DataLayerに突っ込む
+        // ・FeatureはFeatureをまとめて1つの地物にする
+        // トップレベルのtypenameが異なるなら、別のレイヤーにする
+
+        println!("primitive_attributes: {:?}", primitive_attributes);
+        println!("data_list: {:?}", data_list);
+        println!("features: {:?}", features);
+        println!("array_list: {:?}", array_list);
+
+        // featuresが無い→attribute内に子要素がなかった→トップレベルのジオメトリを利用
+
+        println!();
 
         features
     }
 
     pub fn to_tabular(&self, cityobj: &CityObject) -> Vec<Layer> {
-        let manager = LayerManager { layers: None };
-
         let toplevel_feature: &Feature = match &cityobj.root {
             Value::Feature(f) => f,
-            _ => todo!(),
+            _ => panic!(
+                "Root value type must be Feature, but found {:?}",
+                cityobj.root
+            ),
         };
         let parent_gml_id = &toplevel_feature.id;
-        println!("{:?}", parent_gml_id);
+        let typename = &toplevel_feature.typename;
+        println!("{:?}, {:?}", parent_gml_id, typename);
 
         let features = &self.attribute_parser(toplevel_feature, parent_gml_id.clone().unwrap());
 
