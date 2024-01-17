c615419beabefea666bd6608aa10ea1bda0323d8
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-17 19:40:17 +0900

    tmp

--- nusamai/src/transform/city_object.rs
+++ nusamai/src/transform/city_object.rs
@@ -25,43 +25,27 @@ pub struct Settings {
 }
 
 pub trait Transformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject>;
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject>;
 }
 
-pub struct FeatureCollectTransformer {
-    settings: Settings,
-}
+pub struct FeatureCollectTransformer {}
 impl Transformer for FeatureCollectTransformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
         let mut results = Vec::new();
+        let mut child_geometry_refs = Vec::new();
 
-        for o in &city_objects {
-            let feature_ref = match &o.root {
-                Value::Feature(f) => f,
-                _ => panic!("Root value type must be Feature, but found {:?}", o.root),
-            };
-
-            // feature.geometriesから、lodごとに分割して、objectsに追加する
-            if let Value::Feature(f) = &o.root {
-                if let Some(geometry_ref_list) = &f.geometries {
-                    for g in geometry_ref_list.iter() {
-                        let f = Feature {
-                            id: feature_ref.id.clone(),
-                            typename: feature_ref.typename.clone(),
-                            attributes: feature_ref.attributes.clone(),
-                            geometries: Some(vec![g.clone()]),
-                        };
-
-                        let obj = CityObject {
-                            root: Value::Feature(f),
-                            geometry_store: o.geometry_store.clone(),
-                        };
-
-                        results.push(obj);
-                    }
-                }
-            }
+        // 仮実装: 渡されるcity_objectsは、元々同じCityObjectであるため、すべて同じroot_gml_idを持つ
+        if city_objects.is_empty() {
+            return city_objects;
         }
+        let toplevel_city_object = &city_objects[0];
+        let toplevel_feature = match &toplevel_city_object.root {
+            Value::Feature(f) => f.clone(),
+            _ => panic!(
+                "Root value type must be Feature, but found {:?}",
+                city_objects[0].root
+            ),
+        };
 
         // attributes内のFeature（子要素）を全て取り出す
         for o in &city_objects {
@@ -76,60 +60,69 @@ impl Transformer for FeatureCollectTransformer {
                 child_features.extend(features);
             }
 
-            // 設定に応じてfeaturesをセマンティックごとに分割する
-            if self.settings.load_semantic_parts {
-                for f in &child_features {
-                    let obj = CityObject {
-                        root: Value::Feature(f.clone()),
-                        geometry_store: o.geometry_store.clone(),
-                    };
-                    println!("{:?}", obj);
-                    results.push(obj);
+            // child_features内のgeometriesを全て取り出して、toplevel_featureのgeometriesに追加する
+            for f in &child_features {
+                if let Some(geometry_refs) = &f.geometries {
+                    child_geometry_refs.extend(geometry_refs.clone());
                 }
-            } else {
-                // セマンティックごとに分割しない場合は、child_features内のgeometriesを全て取り出して、toplevel_featureのgeometriesに追加する
-                let mut child_geometry_refs = Vec::new();
-                for f in &child_features {
-                    if let Some(geometry_refs) = &f.geometries {
-                        child_geometry_refs.extend(geometry_refs.clone());
+            }
+        }
+
+        // child_geometry_refの重複を除去する
+        let child_geometry_refs: Vec<GeometryRefEntry> =
+            child_geometry_refs
+                .into_iter()
+                .fold(Vec::new(), |mut acc, x| {
+                    if !acc.contains(&x) {
+                        acc.push(x);
                     }
-                }
+                    acc
+                });
 
-                if !child_geometry_refs.is_empty() {
-                    let mut lods: IndexMap<usize, Vec<GeometryRefEntry>> = IndexMap::new();
+        let feature = Feature {
+            id: toplevel_feature.id.clone(),
+            typename: toplevel_feature.typename.clone(),
+            attributes: toplevel_feature.attributes.clone(),
+            geometries: Some(child_geometry_refs.clone()),
+        };
 
-                    for g in &child_geometry_refs {
-                        if g.lod == 0 {
-                            lods.entry(0).or_insert_with(Vec::new).push(g.clone());
-                        } else if g.lod == 1 {
-                            lods.entry(1).or_insert_with(Vec::new).push(g.clone());
-                        } else if g.lod == 2 {
-                            lods.entry(2).or_insert_with(Vec::new).push(g.clone());
-                        } else if g.lod == 3 {
-                            lods.entry(3).or_insert_with(Vec::new).push(g.clone());
-                        } else if g.lod == 4 {
-                            lods.entry(4).or_insert_with(Vec::new).push(g.clone());
-                        }
-                    }
+        let obj = CityObject {
+            root: Value::Feature(feature),
+            geometry_store: toplevel_city_object.geometry_store.clone(),
+        };
+        results.push(obj);
 
-                    let root = feature_ref.clone();
+        results
+    }
+}
 
-                    for (_, geometry_refs) in lods.iter() {
-                        let feature = Feature {
-                            id: root.id.clone(),
-                            typename: root.typename.clone(),
-                            attributes: root.attributes.clone(),
-                            geometries: Some(geometry_refs.clone()),
-                        };
+pub struct SemanticSplitTransformer {}
+impl Transformer for SemanticSplitTransformer {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
+        let mut results = Vec::new();
 
-                        let obj = CityObject {
-                            root: Value::Feature(feature),
-                            geometry_store: o.geometry_store.clone(),
-                        };
+        // 仮実装: FeatureCollectTransformerとは両立しない
 
-                        results.push(obj);
-                    }
-                }
+        // attributes内のFeature（子要素）を全て取り出す
+        for o in &city_objects {
+            let feature_ref = match &o.root {
+                Value::Feature(f) => f,
+                _ => panic!("Root value type must be Feature, but found {:?}", o.root),
+            };
+
+            let mut child_features = Vec::new();
+            for (_, value) in feature_ref.attributes.iter() {
+                let features = extract_features(value);
+                child_features.extend(features);
+            }
+
+            // featuresをセマンティックごとに分割する
+            for f in &child_features {
+                let obj = CityObject {
+                    root: Value::Feature(f.clone()),
+                    geometry_store: o.geometry_store.clone(),
+                };
+                results.push(obj);
             }
         }
 
@@ -137,13 +130,27 @@ impl Transformer for FeatureCollectTransformer {
     }
 }
 
-pub struct FlattenTreeTransformer {
-    settings: Settings,
-}
+pub struct FlattenTreeTransformer {}
 impl Transformer for FlattenTreeTransformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
         let mut results = Vec::new();
+        let mut other_layer_data_list = Vec::new();
 
+        // 仮実装: 渡されるcity_objectsは、元々同じCityObjectであるため、すべて同じroot_gml_idを持つ
+        if city_objects.is_empty() {
+            return city_objects;
+        }
+        let toplevel_feature = match &city_objects[0].root {
+            Value::Feature(f) => f.clone(),
+            _ => panic!(
+                "Root value type must be Feature, but found {:?}",
+                city_objects[0].root
+            ),
+        };
+        let root_gml_id = &toplevel_feature.id;
+        let geometry_store = city_objects[0].geometry_store.clone();
+
+        // attributes内のArrayを取り出し、中身がData（子要素）で、なおかつattributesが複数のkeyを持つものを全て取り出す
         for o in &city_objects {
             let feature_ref = match &o.root {
                 Value::Feature(f) => f,
@@ -151,8 +158,6 @@ impl Transformer for FlattenTreeTransformer {
             };
             let root_gml_id = &feature_ref.id;
 
-            // attributes内のArrayを取り出し、中身がData（子要素）で、なおかつattributesが複数のkeyを持つものを全て取り出す
-            let mut other_layer_data_list = Vec::new();
             for (_, value) in feature_ref.attributes.iter() {
                 let array = extract_array(value);
                 for v in array {
@@ -164,33 +169,43 @@ impl Transformer for FlattenTreeTransformer {
                     }
                 }
             }
-
-            // other_layer_data_listを利用する
-            if self.settings.to_tabular {
-                for d in &other_layer_data_list {
-                    let mut attributes = IndexMap::with_hasher(RandomState::new());
-                    for (key, value) in d.attributes.iter() {
-                        attributes.insert(key.clone(), value.clone());
-                    }
-
-                    let feature = Feature {
-                        id: root_gml_id.clone(),
-                        typename: d.typename.clone(),
-                        attributes,
-                        geometries: None,
-                    };
-
-                    let obj = CityObject {
-                        root: Value::Feature(feature),
-                        geometry_store: o.geometry_store.clone(),
-                    };
-
-                    results.push(obj);
-                }
-            }
         }
 
-        for o in city_objects.clone() {
+        // other_layer_data_listに重複があれば除去する
+        let other_layer_data_list: Vec<Data> =
+            other_layer_data_list
+                .into_iter()
+                .fold(Vec::new(), |mut acc, x| {
+                    if !acc.contains(&x) {
+                        acc.push(x);
+                    }
+                    acc
+                });
+        // println!("other_layer_data_list: {:?}", other_layer_data_list);
+
+        // other_layer_data_listを利用して、city_objectsを再構築する
+        for d in &other_layer_data_list {
+            let mut attributes = IndexMap::with_hasher(RandomState::new());
+            for (key, value) in d.attributes.iter() {
+                attributes.insert(key.clone(), value.clone());
+            }
+
+            let feature = Feature {
+                id: root_gml_id.clone(),
+                typename: d.typename.clone(),
+                attributes,
+                geometries: None,
+            };
+
+            let obj = CityObject {
+                root: Value::Feature(feature),
+                geometry_store: geometry_store.clone(),
+            };
+
+            results.push(obj);
+        }
+
+        for o in city_objects {
             let obj = CityObject {
                 root: o.root.clone(),
                 geometry_store: o.geometry_store.clone(),
@@ -202,53 +217,100 @@ impl Transformer for FlattenTreeTransformer {
     }
 }
 
+struct SeparateLodTransformer {}
+impl Transformer for SeparateLodTransformer {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
+        let mut results = Vec::new();
+
+        for o in &city_objects {
+            let feature_ref = match &o.root {
+                Value::Feature(f) => f,
+                _ => panic!("Root value type must be Feature, but found {:?}", o.root),
+            };
+
+            // feature.geometriesから、lodごとに分割して、objectsに追加する
+            if let Value::Feature(f) = &o.root {
+                if let Some(geometry_ref_list) = &f.geometries {
+                    for g in geometry_ref_list.iter() {
+                        let mut lods: IndexMap<usize, Vec<GeometryRefEntry>> = IndexMap::new();
+
+                        if g.lod == 0 {
+                            lods.entry(0).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 1 {
+                            lods.entry(1).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 2 {
+                            lods.entry(2).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 3 {
+                            lods.entry(3).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 4 {
+                            lods.entry(4).or_insert_with(Vec::new).push(g.clone());
+                        }
+
+                        let root = feature_ref.clone();
+
+                        for (_, geometry_refs) in lods.iter() {
+                            let feature = Feature {
+                                id: root.id.clone(),
+                                typename: root.typename.clone(),
+                                attributes: root.attributes.clone(),
+                                geometries: Some(geometry_refs.clone()),
+                            };
+
+                            let obj = CityObject {
+                                root: Value::Feature(feature),
+                                geometry_store: o.geometry_store.clone(),
+                            };
+
+                            results.push(obj);
+                        }
+                    }
+                }
+            }
+        }
+
+        results
+    }
+}
+
 struct FilterFeaturesTransformer {}
 impl Transformer for FilterFeaturesTransformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
         todo!();
     }
 }
 
 struct FilterAttributesTransformer {}
 impl Transformer for FilterAttributesTransformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
         todo!();
     }
 }
 
 struct AttributesTransformer {}
 impl Transformer for AttributesTransformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
-        todo!();
-    }
-}
-
-struct FilterLODTransformer {}
-impl Transformer for FilterLODTransformer {
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
         todo!();
     }
 }
 
 struct TransformerPipeline {
     transformers: Vec<Box<dyn Transformer>>,
-    settings: Settings,
 }
 impl TransformerPipeline {
-    fn new(transformers: Vec<Box<dyn Transformer>>, settings: Settings) -> Self {
-        Self {
-            transformers,
-            settings,
-        }
+    fn new(transformers: Vec<Box<dyn Transformer>>) -> Self {
+        Self { transformers }
     }
 
-    fn transform(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
-        let mut results = Vec::new();
+    fn add(&mut self, transformer: Box<dyn Transformer>) {
+        self.transformers.push(transformer);
+    }
+
+    fn transform(&self, city_objects: Vec<CityObject>) -> Vec<CityObject> {
+        let mut objects = city_objects;
         for transformer in &self.transformers {
-            let new_objects = transformer.transform(city_objects.clone());
-            results = new_objects;
+            objects = transformer.transform(objects);
         }
-        results
+        objects
     }
 }
 
@@ -268,7 +330,6 @@ impl ObjectTransformer {
         };
         let root_gml_id = &toplevel_feature.id;
         let typename = &toplevel_feature.typename;
-        println!("{:?}, {:?}", root_gml_id, typename);
 
         // 仮の設定を作成する
         let mut settings = Settings::default();
@@ -276,18 +337,25 @@ impl ObjectTransformer {
         settings.to_tabular = true;
         settings.to_json_string = true;
 
-        let transformer_pipeline = TransformerPipeline::new(
-            vec![
-                Box::new(FeatureCollectTransformer {
-                    settings: settings.clone(),
-                }),
-                Box::new(FlattenTreeTransformer {
-                    settings: settings.clone(),
-                }),
-            ],
-            settings.clone(),
-        );
-        let mut objects = transformer_pipeline.transform(vec![cityobj]);
+        let mut transformer_pipeline = TransformerPipeline {
+            transformers: Vec::new(),
+        };
+
+        transformer_pipeline.add(Box::new(FeatureCollectTransformer {}));
+        // if settings.load_semantic_parts {
+        //     transformer_pipeline.add(Box::new(SemanticSplitTransformer {}));
+        // } else {
+        //     transformer_pipeline.add(Box::new(SeparateLodTransformer {}));
+        // }
+        // if settings.to_tabular {
+        //     transformer_pipeline.add(Box::new(FlattenTreeTransformer {}));
+        // }
+
+        let obj = CityObject {
+            root: Value::Feature(toplevel_feature),
+            geometry_store: cityobj.geometry_store.clone(),
+        };
+        let mut objects = transformer_pipeline.transform(vec![obj]);
 
         // Array・Data・featureは全てJSON文字列に変換するかどうか
         if settings.to_json_string {
@@ -336,14 +404,17 @@ impl ObjectTransformer {
         // todo: 上記の設定の内容を検討する
         // todo: プログラムをもう少し構造化する
 
-        for o in &objects {
-            if let Value::Feature(f) = &o.root {
-                println!("{:?}: {:?}", f.id, f.geometries);
-            }
+        if objects.len() > 0 {
+            for o in &objects {
+                if let Value::Feature(f) = &o.root {
+                    println!("{:?}: {:?}", f.id, f.geometries);
+                }
 
-            let file = std::fs::File::create("/Users/satoru/Downloads/output/test.json").unwrap();
-            let mut writer = std::io::BufWriter::new(file);
-            serde_json::to_writer_pretty(writer, &objects).unwrap();
+                let file =
+                    std::fs::File::create("/Users/satoru/Downloads/output/test.json").unwrap();
+                let writer = std::io::BufWriter::new(file);
+                serde_json::to_writer_pretty(writer, &objects).unwrap();
+            }
         }
 
         println!();
