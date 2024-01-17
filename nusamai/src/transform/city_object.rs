3441d53b28df041975fbd5e9f6ac3efe60774871
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-17 16:33:03 +0900

    define feature collector

--- nusamai/src/transform/city_object.rs
+++ nusamai/src/transform/city_object.rs
@@ -8,7 +8,7 @@ use nusamai_citygml::{
 };
 
 // 以下、仮実装
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
 enum SettingValue {
     Json(String),
     Separate(bool),
@@ -16,7 +16,7 @@ enum SettingValue {
     None,
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct Settings {
     load_semantic_parts: bool,
     to_json_string: bool,
@@ -25,13 +25,115 @@ pub struct Settings {
 }
 
 pub trait ObjectTreeTraversal {
-    fn collect(&self, cityobj: &CityObject) -> Vec<CityObject>;
+    fn collect(&self, cityobj: Vec<&CityObject>) -> Vec<CityObject>;
 }
 
-pub struct FeatureTraversal {}
+pub struct FeatureTraversal {
+    settings: Settings,
+}
 impl ObjectTreeTraversal for FeatureTraversal {
-    fn collect(&self, cityobj: &CityObject) -> Vec<CityObject> {
-        todo!();
+    fn collect(&self, city_objects: Vec<&CityObject>) -> Vec<CityObject> {
+        let mut results = Vec::new();
+
+        for o in &city_objects {
+            let feature_ref = match &o.root {
+                Value::Feature(f) => f,
+                _ => panic!("Root value type must be Feature, but found {:?}", o.root),
+            };
+
+            // feature.geometriesから、lodごとに分割して、objectsに追加する
+            if let Value::Feature(f) = &o.root {
+                if let Some(geometry_ref_list) = &f.geometries {
+                    for g in geometry_ref_list.iter() {
+                        let f = Feature {
+                            id: feature_ref.id.clone(),
+                            typename: feature_ref.typename.clone(),
+                            attributes: feature_ref.attributes.clone(),
+                            geometries: Some(vec![g.clone()]),
+                        };
+
+                        let obj = CityObject {
+                            root: Value::Feature(f),
+                            geometry_store: o.geometry_store.clone(),
+                        };
+
+                        results.push(obj);
+                    }
+                }
+            }
+        }
+
+        // attributes内のFeature（子要素）を全て取り出す
+        for o in &city_objects {
+            let feature_ref = match &o.root {
+                Value::Feature(f) => f,
+                _ => panic!("Root value type must be Feature, but found {:?}", o.root),
+            };
+
+            let mut child_features = Vec::new();
+            for (_, value) in feature_ref.attributes.iter() {
+                let features = extract_features(value);
+                child_features.extend(features);
+            }
+
+            // 設定に応じてfeaturesをセマンティックごとに分割する
+            if self.settings.load_semantic_parts {
+                for f in &child_features {
+                    let obj = CityObject {
+                        root: Value::Feature(f.clone()),
+                        geometry_store: o.geometry_store.clone(),
+                    };
+                    println!("{:?}", obj);
+                    results.push(obj);
+                }
+            } else {
+                // セマンティックごとに分割しない場合は、child_features内のgeometriesを全て取り出して、toplevel_featureのgeometriesに追加する
+                let mut child_geometry_refs = Vec::new();
+                for f in &child_features {
+                    if let Some(geometry_refs) = &f.geometries {
+                        child_geometry_refs.extend(geometry_refs.clone());
+                    }
+                }
+
+                if !child_geometry_refs.is_empty() {
+                    let mut lods: IndexMap<usize, Vec<GeometryRefEntry>> = IndexMap::new();
+
+                    for g in &child_geometry_refs {
+                        if g.lod == 0 {
+                            lods.entry(0).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 1 {
+                            lods.entry(1).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 2 {
+                            lods.entry(2).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 3 {
+                            lods.entry(3).or_insert_with(Vec::new).push(g.clone());
+                        } else if g.lod == 4 {
+                            lods.entry(4).or_insert_with(Vec::new).push(g.clone());
+                        }
+                    }
+
+                    let root = feature_ref.clone();
+
+                    for (_, geometry_refs) in lods.iter() {
+                        let feature = Feature {
+                            id: root.id.clone(),
+                            typename: root.typename.clone(),
+                            attributes: root.attributes.clone(),
+                            geometries: Some(geometry_refs.clone()),
+                        };
+
+                        let obj = CityObject {
+                            root: Value::Feature(feature),
+                            geometry_store: o.geometry_store.clone(),
+                        };
+
+                        results.push(obj);
+                    }
+                }
+            }
+        }
+
+        results
     }
 }
 
@@ -119,94 +221,99 @@ impl Transformer for SemanticObjectSeparator {
         let typename = &toplevel_feature.typename;
         println!("{:?}, {:?}", root_gml_id, typename);
 
-        // 返却用の入れ物を用意する
-        let mut objects = Vec::new();
+        // // デフォルトで、LODごとに分割してobjectsに追加する
+        // if let Some(f) = &toplevel_feature.geometries {
+        //     for g in f.iter() {
+        //         let toplevel_feature = Feature {
+        //             id: toplevel_feature.id.clone(),
+        //             typename: toplevel_feature.typename.clone(),
+        //             attributes: toplevel_feature.attributes.clone(),
+        //             geometries: Some(vec![g.clone()]),
+        //         };
 
-        // デフォルトで、LODごとに分割してobjectsに追加する
-        if let Some(f) = &toplevel_feature.geometries {
-            for g in f.iter() {
-                let toplevel_feature = Feature {
-                    id: toplevel_feature.id.clone(),
-                    typename: toplevel_feature.typename.clone(),
-                    attributes: toplevel_feature.attributes.clone(),
-                    geometries: Some(vec![g.clone()]),
-                };
+        //         let obj = CityObject {
+        //             root: Value::Feature(toplevel_feature),
+        //             geometry_store: cityobj.geometry_store.clone(),
+        //         };
 
-                let obj = CityObject {
-                    root: Value::Feature(toplevel_feature),
-                    geometry_store: cityobj.geometry_store.clone(),
-                };
-
-                objects.push(obj);
-            }
-        }
+        //         objects.push(obj);
+        //     }
+        // }
 
         // 仮の設定を作成する
         let mut settings = Settings::default();
+        settings.load_semantic_parts = false;
+        settings.to_tabular = true;
+        settings.to_json_string = true;
+
+        let traversal = FeatureTraversal {
+            settings: settings.clone(),
+        };
+        let mut objects = traversal.collect(vec![cityobj]);
 
         // attributes内のFeature（子要素）を全て取り出す
-        let mut child_features = Vec::new();
-        for (key, value) in toplevel_feature.attributes.iter() {
-            let features = extract_features(value);
-            child_features.extend(features);
-        }
+        // let mut child_features = Vec::new();
+        // for (key, value) in toplevel_feature.attributes.iter() {
+        //     let features = extract_features(value);
+        //     child_features.extend(features);
+        // }
 
         // 設定に応じてfeaturesをセマンティックごとに分割する
-        settings.load_semantic_parts = false;
-        if settings.load_semantic_parts {
-            for f in &child_features {
-                let obj = CityObject {
-                    root: Value::Feature(f.clone()),
-                    geometry_store: cityobj.geometry_store.clone(),
-                };
-                objects.push(obj);
-            }
-        } else {
-            // セマンティックごとに分割しない場合は、child_features内のgeometriesを全て取り出して、toplevel_featureのgeometriesに追加する
-            let mut child_geometry_refs = Vec::new();
-            for f in &child_features {
-                if let Some(geometry_refs) = &f.geometries {
-                    // lodを確認する
-                    child_geometry_refs.extend(geometry_refs.clone());
-                }
-            }
+        // settings.load_semantic_parts = false;
+        // if settings.load_semantic_parts {
+        //     for f in &child_features {
+        //         let obj = CityObject {
+        //             root: Value::Feature(f.clone()),
+        //             geometry_store: cityobj.geometry_store.clone(),
+        //         };
+        //         objects.push(obj);
+        //     }
+        // } else {
+        //     // セマンティックごとに分割しない場合は、child_features内のgeometriesを全て取り出して、toplevel_featureのgeometriesに追加する
+        //     let mut child_geometry_refs = Vec::new();
+        //     for f in &child_features {
+        //         if let Some(geometry_refs) = &f.geometries {
+        //             // lodを確認する
+        //             child_geometry_refs.extend(geometry_refs.clone());
+        //         }
+        //     }
 
-            if !child_geometry_refs.is_empty() {
-                let mut lods: IndexMap<usize, Vec<GeometryRefEntry>> = IndexMap::new();
+        //     if !child_geometry_refs.is_empty() {
+        //         let mut lods: IndexMap<usize, Vec<GeometryRefEntry>> = IndexMap::new();
 
-                for g in &child_geometry_refs {
-                    if g.lod == 0 {
-                        lods.entry(0).or_insert_with(Vec::new).push(g.clone());
-                    } else if g.lod == 1 {
-                        lods.entry(1).or_insert_with(Vec::new).push(g.clone());
-                    } else if g.lod == 2 {
-                        lods.entry(2).or_insert_with(Vec::new).push(g.clone());
-                    } else if g.lod == 3 {
-                        lods.entry(3).or_insert_with(Vec::new).push(g.clone());
-                    } else if g.lod == 4 {
-                        lods.entry(4).or_insert_with(Vec::new).push(g.clone());
-                    }
-                }
+        //         for g in &child_geometry_refs {
+        //             if g.lod == 0 {
+        //                 lods.entry(0).or_insert_with(Vec::new).push(g.clone());
+        //             } else if g.lod == 1 {
+        //                 lods.entry(1).or_insert_with(Vec::new).push(g.clone());
+        //             } else if g.lod == 2 {
+        //                 lods.entry(2).or_insert_with(Vec::new).push(g.clone());
+        //             } else if g.lod == 3 {
+        //                 lods.entry(3).or_insert_with(Vec::new).push(g.clone());
+        //             } else if g.lod == 4 {
+        //                 lods.entry(4).or_insert_with(Vec::new).push(g.clone());
+        //             }
+        //         }
 
-                let mut root = toplevel_feature.clone();
+        //         let mut root = toplevel_feature.clone();
 
-                for (lod, geometry_refs) in lods.iter() {
-                    let feature = Feature {
-                        id: root.id.clone(),
-                        typename: root.typename.clone(),
-                        attributes: root.attributes.clone(),
-                        geometries: Some(geometry_refs.clone()),
-                    };
+        //         for (lod, geometry_refs) in lods.iter() {
+        //             let feature = Feature {
+        //                 id: root.id.clone(),
+        //                 typename: root.typename.clone(),
+        //                 attributes: root.attributes.clone(),
+        //                 geometries: Some(geometry_refs.clone()),
+        //             };
 
-                    let obj = CityObject {
-                        root: Value::Feature(feature),
-                        geometry_store: cityobj.geometry_store.clone(),
-                    };
+        //             let obj = CityObject {
+        //                 root: Value::Feature(feature),
+        //                 geometry_store: cityobj.geometry_store.clone(),
+        //             };
 
-                    objects.push(obj);
-                }
-            }
-        }
+        //             objects.push(obj);
+        //         }
+        //     }
+        // }
 
         // attributes内のArrayを取り出し、中身がData（子要素）で、なおかつattributesが複数のkeyを持つものを全て取り出す
         let mut other_layer_data_list = Vec::new();
@@ -223,7 +330,6 @@ impl Transformer for SemanticObjectSeparator {
         }
 
         // other_layer_data_listを利用する
-        settings.to_tabular = true;
         if settings.to_tabular {
             for d in &other_layer_data_list {
                 let mut attributes = IndexMap::with_hasher(RandomState::new());
@@ -248,7 +354,6 @@ impl Transformer for SemanticObjectSeparator {
         }
 
         // Array・Data・featureは全てJSON文字列に変換するかどうか
-        settings.to_json_string = true;
         if settings.to_json_string {
             for _ in 0..objects.len() {
                 let object = objects.remove(0);
