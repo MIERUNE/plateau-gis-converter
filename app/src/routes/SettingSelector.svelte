191b0e4cdac82b413a2bad72c97a7a289406f489
Sorami Hisamoto <hisamoto@mierune.co.jp>
2024-01-23 10:39:36 +0900

    UIの変更, 複数フォルダ/ファイルの選択, Aboutページの追加 (#179)
    related #138 
    
    ## 動作方法
    
    ```
    $ cd app
    $ npm install
    $ npm run tauri dev
    ```
    
    ## 詳細
    
    主にUI面での変更なので、コードの細かい部分というより、動作確認をしていただけると良いかと思います！ 🙏 
    
    - [x] 全体的な配置調整、現在不要な要素の削除
    - [x] 複数フォルダ, 複数ファイルの選択
    - [x] Aboutページの追加（右上の(i)ボタンから）
    - [x] iconifyの導入（アイコンコンポーネント）
    - [x] ローディングアニメーション（参考: [CSS
    Loaders](https://codepen.io/mjcabangon/pen/pKRaZQ)）（現在は進捗のパーセンテージとかわからないのでとりあえず）
    
    
    ![image](https://github.com/MIERUNE/nusamai/assets/595008/43964231-35f6-45bf-86bf-335521005255)

---
+++ app/src/routes/SettingSelector.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { fileTypeOptions, crsOptions } from '$lib/settings';
+
+	export let filetype: string;
+</script>
+
+<div>
+	<div class="flex items-center gap-1.5">
+		<Icon class="text-xl" icon="material-symbols:settings" />
+		<h2 class="font-bold text-xl">設定</h2>
+	</div>
+	<hr class="mt-0.5" />
+
+	<div class="flex flex-col gap-5 mt-3 ml-2">
+		<div class=" flex flex-col gap-1.5">
+			<label for="filetype-select" class="font-bold">ファイル形式</label>
+			<select bind:value={filetype} name="filetype" id="filetype-select" class="w-36">
+				{#each fileTypeOptions as ft}
+					<option value={ft}>{ft}</option>
+				{/each}
+			</select>
+		</div>
+
+		<div class=" flex flex-col gap-1.5 opacity-50">
+			<label for="crs-select" class="font-bold">座標参照系</label>
+			<select name="crs" id="crs-select" class="w-80">
+				{#each crsOptions as crs}
+					<option value={crs.value}>{crs.label}</option>
+				{/each}
+			</select>
+		</div>
+
+		<div class=" flex flex-col gap-1.5 opacity-50">
+			<label for="crs-select" class="font-bold">属性マッピング</label>
+			<div class="flex items-center gap-3">
+				<button class="bg-accent1 font-semibold rounded px-4 py-0.5 shadow hover:opacity-75"
+					>選択</button
+				>
+				<div class="text-sm">設定ファイルが選択されていません</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+</style>
