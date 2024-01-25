c85e1984626f3fc2f284be2c1fb616de94a6532a
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-25 17:33:31 +0900

    fix

--- nusamai-czml/src/models/mod.rs
+++ nusamai-czml/src/models/mod.rs
@@ -1,16 +1,16 @@
 pub mod czml;
+pub mod czml_string;
 pub mod deletable_property;
 pub mod packet;
-pub mod string_object;
 pub mod tileset;
 pub mod uri;
 pub mod value_properties;
 pub mod values;
 
 pub use czml::*;
+pub use czml_string::*;
 pub use deletable_property::*;
 pub use packet::*;
-pub use string_object::*;
 pub use tileset::*;
 pub use uri::*;
 pub use value_properties::*;
--- nusamai-czml/src/models/string_object.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct StringObject {
-    string: Option<String>,
-    reference: Option<String>,
-}
