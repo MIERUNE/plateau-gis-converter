6ae813fd48910bf584cf9b9f25d4e79eb6a2e55a
Sorami Hisamoto <hisamoto@mierune.co.jp>
2024-01-25 16:27:15 +0900

    (tmp) examples

---
+++ nusamai-shapefile/examples/write.rs
@@ -0,0 +1,29 @@
+use shapefile::dbase;
+
+fn main() {
+    // Attribute fields for the features
+    // FieldName byte representation cannot exceed 11 bytes
+    let table_builder = dbase::TableWriterBuilder::new()
+        .add_character_field("name".try_into().unwrap(), 50)
+        .add_float_field("lon".try_into().unwrap(), 50, 10)
+        .add_float_field("lat".try_into().unwrap(), 50, 10);
+
+    // Create all the files needed for the shapefile to be complete (.shp, .shx, .dbf)
+    let mut writer =
+        shapefile::Writer::from_path("/Users/sorami/Desktop/tmp.shp", table_builder).unwrap();
+
+    for lat in 20..45 {
+        for lon in 120..150 {
+            let shape = shapefile::Point::new(lon as f64, lat as f64);
+            let mut record = dbase::Record::default();
+            record.insert(
+                "name".to_owned(),
+                dbase::FieldValue::Character(Some("foo".to_owned())),
+            );
+            record.insert("lon".to_owned(), dbase::FieldValue::Float(Some(lon as f32)));
+            record.insert("lat".to_owned(), dbase::FieldValue::Float(Some(lat as f32)));
+
+            writer.write_shape_and_record(&shape, &record).unwrap();
+        }
+    }
+}
