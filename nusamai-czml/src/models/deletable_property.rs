0e52fece02be0b572c59c836d31ac6431f90a721
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-25 17:34:01 +0900

    add

--- nusamai-czml/src/models/deletable_property.rs
+++ nusamai-czml/src/models/deletable_property.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct DeletableProperty {}
+pub struct DeletableProperty {
+    #[serde(rename = "delete")]
+    pub delete_flag: bool,
+}
