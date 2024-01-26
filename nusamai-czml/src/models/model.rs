78db972ff16fcd184244fdfb846ac6d5adb94a61
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-26 14:29:53 +0900

    fix

--- nusamai-czml/src/models/model.rs
+++ nusamai-czml/src/models/model.rs
@@ -1,29 +1,29 @@
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    Articulation, Color, ColorBlendMode, DistanceDisplayCondition, HeightReference,
-    NodeTransformation, ShadowMode,
+    Articulation, Color, ColorBlendMode, CzmlBoolean, CzmlDouble, CzmlUri,
+    DistanceDisplayCondition, HeightReference, NodeTransformations, ShadowMode,
 };
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
 pub struct Model {
-    pub show: Option<bool>,
-    pub gltf: String,
-    pub scale: Option<f64>,
-    pub minimum_pixel_size: Option<f64>,
-    pub maximum_scale: Option<f64>,
-    pub incrementally_load_textures: Option<bool>,
-    pub run_animations: Option<bool>,
+    pub show: Option<CzmlBoolean>,
+    pub gltf: CzmlUri,
+    pub scale: Option<CzmlDouble>,
+    pub minimum_pixel_size: Option<CzmlDouble>,
+    pub maximum_scale: Option<CzmlDouble>,
+    pub incrementally_load_textures: Option<CzmlBoolean>,
+    pub run_animations: Option<CzmlBoolean>,
     pub shadows: Option<ShadowMode>,
     pub height_reference: Option<HeightReference>,
     pub silhouette_color: Option<Color>,
-    pub silhouette_size: Option<f64>,
+    pub silhouette_size: Option<CzmlDouble>,
     pub color: Option<Color>,
     pub color_blend_mode: Option<ColorBlendMode>,
-    pub color_blend_amount: Option<f64>,
+    pub color_blend_amount: Option<CzmlDouble>,
     pub distance_display_condition: Option<DistanceDisplayCondition>,
-    pub node_transformations: Option<NodeTransformation>,
+    pub node_transformations: Option<NodeTransformations>,
     pub articulations: Option<Articulation>,
 }
