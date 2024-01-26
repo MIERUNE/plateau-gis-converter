559e03aceb754ada1e3d357ea1aeeca27501616a
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-26 14:27:29 +0900

    define model.rs

--- nusamai-czml/src/models/model.rs
+++ nusamai-czml/src/models/model.rs
@@ -1,4 +1,29 @@
 use serde::{Deserialize, Serialize};
 
+use crate::{
+    Articulation, Color, ColorBlendMode, DistanceDisplayCondition, HeightReference,
+    NodeTransformation, ShadowMode,
+};
+
 #[derive(Serialize, Deserialize)]
-pub struct Model {}
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct Model {
+    pub show: Option<bool>,
+    pub gltf: String,
+    pub scale: Option<f64>,
+    pub minimum_pixel_size: Option<f64>,
+    pub maximum_scale: Option<f64>,
+    pub incrementally_load_textures: Option<bool>,
+    pub run_animations: Option<bool>,
+    pub shadows: Option<ShadowMode>,
+    pub height_reference: Option<HeightReference>,
+    pub silhouette_color: Option<Color>,
+    pub silhouette_size: Option<f64>,
+    pub color: Option<Color>,
+    pub color_blend_mode: Option<ColorBlendMode>,
+    pub color_blend_amount: Option<f64>,
+    pub distance_display_condition: Option<DistanceDisplayCondition>,
+    pub node_transformations: Option<NodeTransformation>,
+    pub articulations: Option<Articulation>,
+}
