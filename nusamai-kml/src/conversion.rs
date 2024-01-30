fe545f54b6b5a4c94a6532b2727962e3fbc21462
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2024-01-30 11:32:18 +0900

    fix consersion

--- nusamai-kml/src/conversion.rs
+++ nusamai-kml/src/conversion.rs
@@ -1,20 +1,45 @@
+use kml::{
+    types::{Geometry, KmlDocument, Placemark, Point},
+    Kml,
+};
 use nusamai_geometry::{CoordNum, MultiPoint};
-use kml::types::Point;
-
+use std::collections::HashMap;
 
 pub fn multipoint_to_kml_with_mapping<const D: usize, T: CoordNum>(
     mpoint: &MultiPoint<D, T>,
     mapping: impl Fn([T; D]) -> [f64; 3],
-) -> Vec<Point> {
-    let kml_points = mpoint
+) -> Vec<Kml> {
+    let points = mpoint
         .iter()
         .map(&mapping)
         .map(|coords| Point::new(coords[0], coords[1], Some(coords[2])))
         .collect::<Vec<_>>();
 
-    kml_points
+    points
+        .into_iter()
+        .map(|pt| {
+            Kml::Placemark(Placemark {
+                geometry: Some(Geometry::Point(pt)),
+                ..Default::default()
+            })
+        })
+        .collect()
 }
 
+pub fn multipoint_to_kml(mpoint: &MultiPoint<3>) -> Kml {
+    let placemarks = multipoint_to_kml_with_mapping(mpoint, |c| c);
+    let folder = Kml::Folder {
+        attrs: HashMap::new(),
+        elements: placemarks,
+    };
+
+    let document = KmlDocument {
+        elements: vec![folder],
+        ..Default::default()
+    };
+
+    Kml::KmlDocument(document)
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,49 +47,59 @@ mod tests {
 
     #[test]
     fn test_multipoint_to_kml() {
-
-        use kml::{Kml, KmlWriter, types::{Placemark, KmlDocument, Geometry}};
-        use std::fs::File;
-        use std::io::{BufWriter, Write};
-        use std::collections::HashMap;
-
         let mut mpoint = MultiPoint::<3>::new();
         mpoint.push(&[11., 12., 13.]);
         mpoint.push(&[21., 22., 23.]);
         mpoint.push(&[31., 32., 33.]);
 
-        let kml_points = multipoint_to_kml_with_mapping(&mpoint, |c| c);
+        let kml = multipoint_to_kml(&mpoint);
 
-        
-        let placemarks: Vec<Kml<_>> = kml_points.into_iter()
-        .map(|pt| Kml::Placemark(Placemark {
-            geometry: Some(Geometry::Point(pt)),
-            ..Default::default()
-        }))
-        .collect();
+        match kml {
+            Kml::KmlDocument(doc) => {
+                assert_eq!(doc.elements.len(), 1);
+                match doc.elements[0] {
+                    Kml::Folder { ref elements, .. } => {
+                        assert_eq!(elements.len(), 3);
+                        match &elements[0] {
+                            Kml::Placemark(Placemark {
+                                geometry: Some(Geometry::Point(Point { coord: coords, .. })),
+                                ..
+                            }) => {
+                                assert_eq!(coords.x, 11.);
+                                assert_eq!(coords.y, 12.);
+                                assert_eq!(coords.z, Some(13.));
+                            }
+                            _ => panic!("Unexpected element type"),
+                        }
 
-        let folder = Kml::Folder {
-            attrs: HashMap::new(),
-            elements: placemarks,
-        };
+                        match &elements[1] {
+                            Kml::Placemark(Placemark {
+                                geometry: Some(Geometry::Point(Point { coord: coords, .. })),
+                                ..
+                            }) => {
+                                assert_eq!(coords.x, 21.);
+                                assert_eq!(coords.y, 22.);
+                                assert_eq!(coords.z, Some(23.));
+                            }
+                            _ => panic!("Unexpected element type"),
+                        }
 
-        let document = KmlDocument {
-            elements: vec![folder],
-            ..Default::default()
-        };
-
-
-        let kml = Kml::KmlDocument(document);
-
-        let file_path = "output.kml";
-        let file = File::create(file_path).expect("Failed to create file");
-
-        let mut buf_writer = BufWriter::new(file);
-
-
-        writeln!(buf_writer, r#"<?xml version="1.0" encoding="UTF-8"?>"#).expect("Failed to write XML declaration");
-        let mut kml_writer = KmlWriter::from_writer(&mut buf_writer);
-        kml_writer.write(&kml).expect("Failed to write KML data");
-            
+                        match &elements[2] {
+                            Kml::Placemark(Placemark {
+                                geometry: Some(Geometry::Point(Point { coord: coords, .. })),
+                                ..
+                            }) => {
+                                assert_eq!(coords.x, 31.);
+                                assert_eq!(coords.y, 32.);
+                                assert_eq!(coords.z, Some(33.));
+                            }
+                            _ => panic!("Unexpected element type"),
+                        }
+                    }
+                    _ => panic!("Unexpected element type"),
+                }
+            }
+            _ => panic!("Unexpected element type"),
+        }
     }
 }
