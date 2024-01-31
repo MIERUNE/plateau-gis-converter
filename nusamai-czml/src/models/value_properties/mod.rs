b97f7de4fe32f05606f795deb2d53e537fea512b
nokonoko1203 <healtheworld.1203@gmail.com>
2024-01-31 15:53:44 +0900

    Integer

--- nusamai-czml/src/models/value_properties/mod.rs
+++ nusamai-czml/src/models/value_properties/mod.rs
@@ -1,7 +1,9 @@
 pub mod cartesian_3_value_property;
 pub mod double_value_property;
+pub mod integer_value_property;
 pub mod reference_value_property;
 
 pub use cartesian_3_value_property::*;
 pub use double_value_property::*;
+pub use integer_value_property::*;
 pub use reference_value_property::*;
--- nusamai-czml/src/models/values/mod.rs
+++ nusamai-czml/src/models/values/mod.rs
@@ -1,7 +1,9 @@
 pub mod cartesian_3_value;
 pub mod double_value;
+pub mod integer_value;
 pub mod reference_value;
 
 pub use cartesian_3_value::*;
 pub use double_value::*;
+pub use integer_value::*;
 pub use reference_value::*;
