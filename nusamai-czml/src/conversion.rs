d64b060198e252a4be973c6e133e1cd68a965376
nokonoko1203 <healtheworld.1203@gmail.com>
2024-02-01 07:52:45 +0900

    add temporary function

--- nusamai-czml/src/conversion.rs
+++ nusamai-czml/src/conversion.rs
@@ -1,1 +1,31 @@
-use nusamai_geometry::{CoordNum, LineString, MultiLineString, MultiPoint, MultiPolygon, Polygon};
+use nusamai_geometry::{CoordNum, MultiPolygon, Polygon};
+
+use crate::{models::CzmlPolygon, PositionListOfListsProperties, PositionListProperties};
+
+pub fn polygon_to_czml_polygon<const D: usize, T: CoordNum>(
+    polygon: &Polygon<D, T>,
+) -> CzmlPolygon {
+    let mut czml_polygon = CzmlPolygon::default();
+    let mut positions = PositionListProperties {
+        ..Default::default()
+    };
+    let mut holes = PositionListOfListsProperties {
+        ..Default::default()
+    };
+
+    czml_polygon
+}
+
+pub fn multi_polygon_to_czml_polygon<const D: usize, T: CoordNum>(
+    multi_polygon: &MultiPolygon<D, T>,
+) -> CzmlPolygon {
+    let mut czml_polygon = CzmlPolygon::default();
+    let mut positions = PositionListProperties {
+        ..Default::default()
+    };
+    let mut holes = PositionListOfListsProperties {
+        ..Default::default()
+    };
+
+    czml_polygon
+}
