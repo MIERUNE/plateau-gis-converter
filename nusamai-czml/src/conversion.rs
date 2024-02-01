2505ffdc60e51161ebb57c44caf6da15b87e93ad
nokonoko1203 <healtheworld.1203@gmail.com>
2024-02-01 13:20:18 +0900

    add test

--- nusamai-czml/src/conversion.rs
+++ nusamai-czml/src/conversion.rs
@@ -2,11 +2,28 @@ use nusamai_geometry::Polygon;
 
 use crate::models::CzmlPolygon;
 
-pub fn multi_polygon_to_czml_polygon(
+//
+pub fn indexed_multipolygon_to_czml_polygon(
     vertices: &[[f64; 3]],
     poly_idx: &Polygon<1, u32>,
 ) -> CzmlPolygon {
     let mut czml_polygon = CzmlPolygon::default();
 
+    let coords = poly_idx
+        .exterior()
+        .iter()
+        .map(|&idx| vertices[idx as usize])
+        .collect::<Vec<_>>();
+
     czml_polygon
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut poly = Polygon::new();
+    }
 }
