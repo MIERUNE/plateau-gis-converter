39c0b6c3fcf7ca8c9072595b76b797a10700a04a
nokonoko1203 <healtheworld.1203@gmail.com>
2024-02-01 18:29:16 +0900

    add comment

--- nusamai-czml/src/models/document.rs
+++ nusamai-czml/src/models/document.rs
@@ -4,5 +4,6 @@ use crate::models::packet::Packet;
 
 #[derive(Serialize, Deserialize)]
 pub struct Document {
+    // In Cesium, it is necessary to always place a Packet like "{"id": "document", "version": "1.0"}" at the beginning.
     pub packets: Vec<Packet>,
 }
