1278363be1ea5e52bf63ebb40f99db69e2c2d075
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2024-02-02 14:18:36 +0900

    multipoint_to_kml_with_mappingではMultiGeometryを戻す

--- nusamai-kml/examples/write.rs
+++ nusamai-kml/examples/write.rs
@@ -1,6 +1,10 @@
-use kml::KmlWriter;
+use kml::{
+    types::{Geometry, Kml, Placemark},
+    KmlWriter,
+};
 use nusamai_geometry::MultiPoint;
 use nusamai_kml::conversion::multipoint_to_kml;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
@@ -10,7 +14,17 @@ fn main() {
     mpoint.push(&[21., 22., 23.]);
     mpoint.push(&[31., 32., 33.]);
 
-    let kml = multipoint_to_kml(&mpoint);
+    let multi_geom = multipoint_to_kml(&mpoint);
+
+    let placemark = Placemark {
+        geometry: Some(Geometry::MultiGeometry(multi_geom)),
+        ..Default::default()
+    };
+
+    let folder = Kml::Folder {
+        attrs: HashMap::new(),
+        elements: vec![Kml::Placemark(placemark)],
+    };
 
     let file_path = "output.kml";
     let file = File::create(file_path).expect("Failed to create file");
@@ -19,6 +33,13 @@ fn main() {
 
     writeln!(buf_writer, r#"<?xml version="1.0" encoding="UTF-8"?>"#)
         .expect("Failed to write XML declaration");
+    writeln!(
+        buf_writer,
+        r#"<kml xmlns="http://www.opengis.net/kml/2.2">"#
+    ) // Add <kml> tag here
+    .expect("Failed to write <kml> tag");
     let mut kml_writer = KmlWriter::from_writer(&mut buf_writer);
-    kml_writer.write(&kml).expect("Failed to write KML data");
+    kml_writer.write(&folder).expect("Failed to write KML data");
+    writeln!(buf_writer, "</kml>") // Add </kml> tag here
+        .expect("Failed to write </kml> tag");
 }
--- nusamai-kml/src/conversion.rs
+++ nusamai-kml/src/conversion.rs
@@ -1,43 +1,30 @@
-use kml::{
-    types::{Geometry, Placemark, Point},
-    Kml,
-};
+use kml::types::{Geometry, MultiGeometry, Point};
 use nusamai_geometry::{CoordNum, MultiPoint};
 use std::collections::HashMap;
 
 pub fn multipoint_to_kml_with_mapping<const D: usize, T: CoordNum>(
     mpoint: &MultiPoint<D, T>,
     mapping: impl Fn([T; D]) -> [f64; 3],
-) -> Kml {
+) -> MultiGeometry {
     let points = mpoint
         .iter()
         .map(&mapping)
         .map(|coords| Point::new(coords[0], coords[1], Some(coords[2])))
         .collect::<Vec<_>>();
-
-    let placemarks = points
-        .into_iter()
-        .map(|pt| {
-            Kml::Placemark(Placemark {
-                geometry: Some(Geometry::Point(pt)),
-                ..Default::default()
-            })
-        })
-        .collect();
-    Kml::Folder {
+    MultiGeometry {
+        geometries: points.into_iter().map(|pt| Geometry::Point(pt)).collect(),
         attrs: HashMap::new(),
-        elements: placemarks,
     }
 }
 
-pub fn multipoint_to_kml(mpoint: &MultiPoint<3>) -> Kml {
+pub fn multipoint_to_kml(mpoint: &MultiPoint<3>) -> MultiGeometry {
     multipoint_to_kml_with_mapping(mpoint, |c| c)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use kml::{types::Placemark, Kml, KmlDocument};
+    use kml::types::{Geometry, Point};
 
     #[test]
     fn test_multipoint_to_kml() {
@@ -46,58 +33,17 @@ mod tests {
         mpoint.push(&[21., 22., 23.]);
         mpoint.push(&[31., 32., 33.]);
 
-        let folder = multipoint_to_kml(&mpoint);
+        let multi_geom = multipoint_to_kml(&mpoint);
 
-        let document = KmlDocument {
-            elements: vec![folder],
-            ..Default::default()
-        };
-        let kml = Kml::KmlDocument(document);
+        assert_eq!(&multi_geom.geometries.len(), &3);
 
-        if let Kml::KmlDocument(doc) = kml {
-            assert_eq!(doc.elements.len(), 1);
-
-            if let Kml::Folder { elements, .. } = &doc.elements[0] {
-                assert_eq!(elements.len(), 3);
-
-                if let Kml::Placemark(Placemark {
-                    geometry: Some(Geometry::Point(Point { coord: coords, .. })),
-                    ..
-                }) = &elements[0]
-                {
-                    assert_eq!(coords.x, 11.);
-                    assert_eq!(coords.y, 12.);
-                    assert_eq!(coords.z, Some(13.));
-                } else {
-                    panic!("Unexpected element type");
-                }
-
-                if let Kml::Placemark(Placemark {
-                    geometry: Some(Geometry::Point(Point { coord: coords, .. })),
-                    ..
-                }) = &elements[1]
-                {
-                    assert_eq!(coords.x, 21.);
-                    assert_eq!(coords.y, 22.);
-                    assert_eq!(coords.z, Some(23.));
-                } else {
-                    panic!("Unexpected element type");
-                }
-
-                if let Kml::Placemark(Placemark {
-                    geometry: Some(Geometry::Point(Point { coord: coords, .. })),
-                    ..
-                }) = &elements[2]
-                {
-                    assert_eq!(coords.x, 31.);
-                    assert_eq!(coords.y, 32.);
-                    assert_eq!(coords.z, Some(33.));
-                } else {
-                    panic!("Unexpected element type");
-                }
-            } else {
-                panic!("Unexpected element type");
-            }
-        }
+        assert_eq!(
+            &multi_geom.geometries,
+            &vec![
+                Geometry::Point(Point::new(11., 12., Some(13.))),
+                Geometry::Point(Point::new(21., 22., Some(23.))),
+                Geometry::Point(Point::new(31., 32., Some(33.)))
+            ]
+        );
     }
 }
