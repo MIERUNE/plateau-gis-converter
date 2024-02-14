e56ac60779b0db7ce1efe5517f7aa5c398825c2a
Sorami Hisamoto <hisamoto@mierune.co.jp>
2024-02-14 23:21:47 +0900

    `prepare_object_attributes()` in a separate file

--- nusamai/src/sink/gpkg/mod.rs
+++ nusamai/src/sink/gpkg/mod.rs
@@ -1,5 +1,6 @@
 //! GeoPackage sink
 
+mod attributes;
 mod bbox;
 mod table;
 
@@ -15,10 +16,11 @@ use crate::parameters::*;
 use crate::pipeline::{Feedback, PipelineError, Receiver, Result};
 use crate::sink::{DataSink, DataSinkProvider, SinkInfo};
 use crate::{get_parameter_value, transformer};
+use attributes::prepare_object_attributes;
 use bbox::{get_indexed_multipolygon_bbox, Bbox};
 use table::schema_to_table_infos;
 
-use nusamai_citygml::object::{Object, ObjectStereotype, Value};
+use nusamai_citygml::object::{ObjectStereotype, Value};
 use nusamai_citygml::schema::Schema;
 use nusamai_citygml::GeometryType;
 use nusamai_gpkg::geometry::write_indexed_multipolygon;
@@ -225,59 +227,6 @@ impl DataSink for GpkgSink {
         let runtime = tokio::runtime::Runtime::new().unwrap();
         runtime.block_on(self.run_async(upstream, feedback, schema))
     }
-}
-
-/// Prepare the attribute values for the GeoPackage
-fn prepare_object_attributes(obj: &Object) -> IndexMap<String, String> {
-    let mut attributes = IndexMap::<String, String>::new();
-
-    for (attr_name, attr_value) in &obj.attributes {
-        match attr_value {
-            Value::String(s) => {
-                attributes.insert(attr_name.into(), s.into());
-            }
-            Value::Code(c) => {
-                // value of the code
-                attributes.insert(attr_name.into(), c.value().into());
-            }
-            Value::Integer(i) => {
-                attributes.insert(attr_name.into(), i.to_string());
-            }
-            Value::NonNegativeInteger(i) => {
-                attributes.insert(attr_name.into(), i.to_string());
-            }
-            Value::Double(d) => {
-                attributes.insert(attr_name.into(), d.to_string());
-            }
-            Value::Measure(m) => {
-                attributes.insert(attr_name.into(), m.value().to_string());
-            }
-            Value::Boolean(b) => {
-                // 0 for false and 1 for true in SQLite
-                attributes.insert(attr_name.into(), if *b { "1".into() } else { "0".into() });
-            }
-            Value::URI(u) => {
-                // value of the URI
-                attributes.insert(attr_name.into(), u.value().to_string());
-            }
-            Value::Date(d) => {
-                // Date represented as an ISO8601 string
-                attributes.insert(attr_name.into(), d.to_string());
-            }
-            Value::Point(_p) => {
-                // TODO: implement
-                // Point struct currently does not contain any data
-            }
-            Value::Array(_arr) => {
-                // TODO: handle multiple values
-            }
-            Value::Object(_obj) => {
-                // TODO: handle nested objects
-            }
-        };
-    }
-
-    attributes
 }
 
 #[cfg(test)]
