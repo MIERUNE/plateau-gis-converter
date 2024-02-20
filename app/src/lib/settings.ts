3445f41f8dafd763285384be745730f8eebb7af1
nokonoko1203 <healtheworld.1203@gmail.com>
2024-02-20 22:23:39 +0900

    add file format

--- app/src/lib/settings.ts
+++ app/src/lib/settings.ts
@@ -11,17 +11,33 @@ const filetypeOptions: Record<string, { label: string; extensions: string[] }> =
 		label: 'Vector Tiles',
 		extensions: ['']
 	},
+	czml: {
+		label: 'CZML',
+		extensions: ['json']
+	},
+	cesiumtiles: {
+		label: '3DTiles',
+		extensions: ['']
+	},
+	kml: {
+		label: 'KML',
+		extensions: ['kml']
+	},
 	shapefile: {
 		label: 'Shapefile',
 		extensions: ['shp']
 	},
+	ply: {
+		label: 'PLY',
+		extensions: ['ply']
+	},
+	glb: {
+		label: 'GLB',
+		extensions: ['glb']
+	},
 	serde: {
 		label: 'Serde',
 		extensions: ['']
-	},
-	czml: {
-		label: 'CZML',
-		extensions: ['json']
 	}
 };
 
