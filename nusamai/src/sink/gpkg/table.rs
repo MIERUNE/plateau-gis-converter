a6daedaf9e81343db791bcd7a7af3da2e48dad23
Sorami Hisamoto <hisamoto@mierune.co.jp>
2024-02-20 14:17:55 +0900

    Move the columninfo creation step to a separete `attribute_to_column()` function. Add columns for the TypeDef::Data

--- nusamai/src/sink/gpkg/table.rs
+++ nusamai/src/sink/gpkg/table.rs
@@ -1,5 +1,5 @@
 use indexmap::IndexMap;
-use nusamai_citygml::schema::{Schema, TypeDef, TypeRef};
+use nusamai_citygml::schema::{Attribute, Schema, TypeDef, TypeRef};
 use nusamai_gpkg::table::{ColumnInfo, TableInfo};
 
 /// Check the schema, and prepare the information for the SQLite table
@@ -25,111 +25,18 @@ fn typedef_to_columns(ty: &TypeDef) -> Vec<ColumnInfo> {
     match ty {
         TypeDef::Feature(feat_td) => {
             // Note: `feat_td.additional_attributes` is expected to be false (handled by the transformer in the earlier step)
-            feat_td.attributes.iter().for_each(|(attr_name, attr)|
-                // Note: `attr.max_occurs` is expected to be 1 (handled by the transformer in the earlier step)
-                match &attr.type_ref {
-                    TypeRef::String => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "TEXT".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Code => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "TEXT".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Integer | TypeRef::NonNegativeInteger => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "INTEGER".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Double => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "REAL".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Boolean => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "BOOLEAN".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::JsonString => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "TEXT".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::URI => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "TEXT".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Date => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "DATE".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::DateTime => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "TEXT".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Measure => {
-                        columns.push(ColumnInfo {
-                            name: attr_name.clone(),
-                            data_type: "REAL".into(),
-                            mime_type: None,
-                        });
-                    }
-                    TypeRef::Point => {
-                        // TODO: implement
-                        // Point struct currently does not contain any data
-                        log::warn!(
-                            "TypeDef::Feature - Unsupported attribute type: {:?} ('{}')",
-                            attr.type_ref,
-                            attr_name
-                        );
-                    }
-                    TypeRef::Named(_name) => {
-                        // TODO: Implement
-                        log::warn!(
-                            "TypeDef::Feature - Unsupported attribute type: {:?} ('{}')",
-                            attr.type_ref,
-                            attr_name
-                        );
-                    },
-                    TypeRef::Unknown => {
-                        log::warn!(
-                            "TypeDef::Feature - Unsupported attribute type: {:?} ('{}')",
-                            attr.type_ref,
-                            attr_name
-                        );
-                    }
-                });
+            feat_td.attributes.iter().for_each(|(attr_name, attr)| {
+                if let Some(column) = attribute_to_column(attr_name, attr) {
+                    columns.push(column);
+                }
+            });
         }
         TypeDef::Data(data_td) => {
-            // TODO: implement
-            log::warn!(
-                "TypeDef::Data - Not supported yet: {:?}",
-                data_td.attributes.values()
-            );
+            data_td.attributes.iter().for_each(|(attr_name, attr)| {
+                if let Some(column) = attribute_to_column(attr_name, attr) {
+                    columns.push(column);
+                }
+            });
         }
         TypeDef::Property(prop_td) => {
             // Note: expected to be handled by the tranformer in the earlier step
@@ -147,4 +54,87 @@ fn typedef_to_columns(ty: &TypeDef) -> Vec<ColumnInfo> {
     };
 
     columns
+}
+
+fn attribute_to_column(attr_name: &str, attr: &Attribute) -> Option<ColumnInfo> {
+    // Note: `attr.max_occurs` is expected to be 1 (handled by the transformer in the earlier step)
+    match &attr.type_ref {
+        TypeRef::String => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "TEXT".into(),
+            mime_type: None,
+        }),
+        TypeRef::Code => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "TEXT".into(),
+            mime_type: None,
+        }),
+        TypeRef::Integer | TypeRef::NonNegativeInteger => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "INTEGER".into(),
+            mime_type: None,
+        }),
+        TypeRef::Double => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "REAL".into(),
+            mime_type: None,
+        }),
+        TypeRef::Boolean => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "BOOLEAN".into(),
+            mime_type: None,
+        }),
+        TypeRef::JsonString => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "TEXT".into(),
+            mime_type: None,
+        }),
+        TypeRef::URI => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "TEXT".into(),
+            mime_type: None,
+        }),
+        TypeRef::Date => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "DATE".into(),
+            mime_type: None,
+        }),
+        TypeRef::DateTime => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "TEXT".into(),
+            mime_type: None,
+        }),
+        TypeRef::Measure => Some(ColumnInfo {
+            name: attr_name.to_string(),
+            data_type: "REAL".into(),
+            mime_type: None,
+        }),
+        TypeRef::Point => {
+            // TODO: implement
+            // Point struct currently does not contain any data
+            log::warn!(
+                "TypeDef::Feature - Unsupported attribute type: {:?} ('{}')",
+                attr.type_ref,
+                attr_name
+            );
+            None
+        }
+        TypeRef::Named(_name) => {
+            // TODO: Implement
+            log::warn!(
+                "TypeDef::Feature - Unsupported attribute type: {:?} ('{}')",
+                attr.type_ref,
+                attr_name
+            );
+            None
+        }
+        TypeRef::Unknown => {
+            log::warn!(
+                "TypeDef::Feature - Unsupported attribute type: {:?} ('{}')",
+                attr.type_ref,
+                attr_name
+            );
+            None
+        }
+    }
 }
