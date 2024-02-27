a22dc74230748d50cfeed6401c6788f0b7973f9d
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2024-02-27 16:44:02 +0900

    fix typo

--- docs/manual/mapping_rules/src/make_mapping_rule_doc.sh
+++ docs/manual/mapping_rules/src/make_mapping_rule_doc.sh
@@ -1,6 +1,6 @@
-for FILE_FORMART in shapefile gpkg 
+for FILE_FOMART in shapefile gpkg 
 do
-    cargo run -p nusamai -- .64413325_bldg_6697_op.gml --schema schema_${FILE_FORMART}.json --sink $FILE_FORMART --output out.dummy
+    cargo run -p nusamai -- .64413325_bldg_6697_op.gml --schema schema_${FILE_FOMART}.json --sink $FILE_FOMART --output out.dummy
 done
 
 python3 schema_to_doc.py
