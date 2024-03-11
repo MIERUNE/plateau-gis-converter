efa4a54a761ca468070169ee380dfedfb90d0768
nokonoko1203 <healtheworld.1203@gmail.com>
2024-03-11 12:06:17 +0900

    Revert "to TEXT type"
    This reverts commit 8df8c22a8c7a6e0ba1cc2e873af5dd72cc859776.

--- nusamai-gpkg/src/handler.rs
+++ nusamai-gpkg/src/handler.rs
@@ -189,11 +189,10 @@ impl<'c> GpkgTransaction<'c> {
         // Create the table
         let mut query_string = format!("CREATE TABLE \"{}\" (", table_info.name);
         if table_info.has_geometry {
-            query_string.push_str("fid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL");
-            query_string.push_str(", id TEXT NOT NULL");
+            query_string.push_str("id STRING NOT NULL PRIMARY KEY");
             query_string.push_str(", geometry BLOB NOT NULL");
         } else {
-            query_string.push_str("fid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL");
+            query_string.push_str("id INTEGER NOT NULL PRIMARY KEY");
         }
         table_info.columns.iter().for_each(|column| {
             query_string.push_str(&format!(", \"{}\" {}", column.name, column.data_type));
@@ -403,7 +402,6 @@ mod tests {
         assert_eq!(
             columns,
             vec![
-                ("fid".into(), "INTEGER".into(), 1),
                 ("id".into(), "STRING".into(), 1),
                 ("geometry".into(), "BLOB".into(), 1),
                 ("attr1".into(), "TEXT".into(), 0),
@@ -477,7 +475,7 @@ mod tests {
             columns,
             vec![
                 // No geometry column
-                ("fid".into(), "INTEGER".into(), 1),
+                ("id".into(), "INTEGER".into(), 1),
                 ("attr1".into(), "TEXT".into(), 0),
             ]
         );
@@ -612,7 +610,7 @@ mod tests {
 
         assert_eq!(rows.len(), 1);
         let row = rows.first().unwrap();
-        assert_eq!(row.get::<i64, &str>("fid"), 1);
+        assert_eq!(row.get::<i64, &str>("id"), 1);
         assert_eq!(row.get::<String, &str>("attr1"), "value1");
         assert_eq!(row.get::<i64, &str>("attr2"), 2);
         assert_eq!(row.get::<f64, &str>("attr3"), 3.33);
