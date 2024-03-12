2b69a05c57e88db8cba6f874e679319414c305ec
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2024-03-12 16:43:09 +0900

    update about page

--- app/src/routes/about/+page.svelte
+++ app/src/routes/about/+page.svelte
@@ -3,18 +3,45 @@
 </script>
 
 <div class="grid place-items-center h-screen">
-	<div class="max-w-md flex flex-col gap-9">
+	<div class="max-w-md flex flex-col gap-7">
 		<h1 class="font-semibold text-lg">PLATEAU 都市デジタルツイン・GISコンバータ</h1>
 
-		<p>国土交通省によるPLATEAUデータを、各種ファイル形式へ変換するためのツールです。</p>
 		<p>
+			国土交通省によるPLATEAUデータを、各種ファイル形式へ変換するためのツールです。
+			<br />
+
 			<a href="https://www.mlit.go.jp/plateau/" target="_blank"
 				>PLATEAU [プラトー] |
 				国土交通省が主導する、日本全国の3D都市モデルの整備・オープンデータ化プロジェクト</a
 			>
 		</p>
+
+		<p>
+			PLATEAUの標準仕様に準拠したCityGML2.0形式の3D都市モデルは、専門のGISツールやCUIツールを用いて他のGIS形式に変換して用いられることが一般的ですが、一般ユーザーが簡易に利用可能な汎用的ツールは存在しません。このため、流通や活用の範囲が専門家や技術者に限られていました。「GISコンバータ」を利用することで、3D都市モデルを用いた様々な分析・開発を行うことができます。
+			<br />・GeoPackage 形式による QGIS 等での解析
+			<br />・Mapbox Vector Tiles (MVT) 形式による、大規模データのWeb等での高速描画
+			<br />・KML 形式による Google Earth での可視化
+			<br />・3D Tiles 形式による Cesium 等での可視化
+			<br />・など
+		</p>
+
 		<p>
 			当ツールの開発は<a href="https://mierune.co.jp" target="_blank">MIERUNE Inc.</a>が行いました。
+		</p>
+
+		<h1 class="font-semibold text-lg">利用手順</h1>
+
+		<p>
+			ソフトウェアの最新版は
+			<a href="https://github.com/MIERUNE/PLATEAU-GIS-Converter/releases" target="_blank"
+				>Releaseページ</a
+			>
+			からダウンロードしてください。
+			<br />
+			詳しい利用方法については、こちらの
+			<a href="https://mierune.github.io/PLATEAU-GIS-Converter/index.html" target="_blank"
+				>マニュアル</a
+			>をご覧ください。
 		</p>
 
 		<div class="mx-auto">
