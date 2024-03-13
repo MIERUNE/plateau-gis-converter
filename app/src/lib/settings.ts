a9783477c487d267e4a149bd571af7270cc359b7
nokonoko1203 <healtheworld.1203@gmail.com>
2024-03-13 16:03:16 +0900

    fix

--- app/src/lib/settings.ts
+++ app/src/lib/settings.ts
@@ -104,19 +104,19 @@ const filetypeOptions: Record<string, { label: string; extensions: string[]; eps
 				{ value: 6685, label: 'JGD 2011 / 平面直角座標系 XVII (EPSG:6685)' },
 				{ value: 6686, label: 'JGD 2011 / 平面直角座標系 XVIII (EPSG:6686)' },
 				{ value: 6687, label: 'JGD 2011 / 平面直角座標系 XIX (EPSG:6687)' },
-				{ value: 10162, label: 'JGD 2011 / 平面直角座標系 I + 標高（EPSG 10162）' },
-				{ value: 10163, label: 'JGD 2011 / 平面直角座標系 II + 標高（EPSG 10163）' },
-				{ value: 10164, label: 'JGD 2011 / 平面直角座標系 III + 標高（EPSG 10164）' },
-				{ value: 10165, label: 'JGD 2011 / 平面直角座標系 IV + 標高（EPSG 10165）' },
-				{ value: 10166, label: 'JGD 2011 / 平面直角座標系 V + 標高（EPSG 10166）' },
-				{ value: 10167, label: 'JGD 2011 / 平面直角座標系 VI + 標高（EPSG 10167）' },
-				{ value: 10168, label: 'JGD 2011 / 平面直角座標系 VII + 標高（EPSG 10168）' },
-				{ value: 10169, label: 'JGD 2011 / 平面直角座標系 VIII + 標高（EPSG 10169）' },
-				{ value: 10170, label: 'JGD 2011 / 平面直角座標系 IX + 標高（EPSG 10170）' },
-				{ value: 10171, label: 'JGD 2011 / 平面直角座標系 X + 標高（EPSG 10171）' },
-				{ value: 10172, label: 'JGD 2011 / 平面直角座標系 XI + 標高（EPSG 10172）' },
-				{ value: 10173, label: 'JGD 2011 / 平面直角座標系 XII + 標高（EPSG 10173）' },
-				{ value: 10174, label: 'JGD 2011 / 平面直角座標系 XIII + 標高（EPSG 10174）' }
+				{ value: 10162, label: 'JGD 2011 / 平面直角座標系 I + 標高 (EPSG:10162)' },
+				{ value: 10163, label: 'JGD 2011 / 平面直角座標系 II + 標高 (EPSG:10163)' },
+				{ value: 10164, label: 'JGD 2011 / 平面直角座標系 III + 標高 (EPSG:10164)' },
+				{ value: 10165, label: 'JGD 2011 / 平面直角座標系 IV + 標高 (EPSG:10165)' },
+				{ value: 10166, label: 'JGD 2011 / 平面直角座標系 V + 標高 (EPSG:10166)' },
+				{ value: 10167, label: 'JGD 2011 / 平面直角座標系 VI + 標高 (EPSG:10167)' },
+				{ value: 10168, label: 'JGD 2011 / 平面直角座標系 VII + 標高 (EPSG:10168)' },
+				{ value: 10169, label: 'JGD 2011 / 平面直角座標系 VIII + 標高 (EPSG:10169)' },
+				{ value: 10170, label: 'JGD 2011 / 平面直角座標系 IX + 標高 (EPSG:10170)' },
+				{ value: 10171, label: 'JGD 2011 / 平面直角座標系 X + 標高 (EPSG:10171)' },
+				{ value: 10172, label: 'JGD 2011 / 平面直角座標系 XI + 標高 (EPSG:10172)' },
+				{ value: 10173, label: 'JGD 2011 / 平面直角座標系 XII + 標高 (EPSG:10173)' },
+				{ value: 10174, label: 'JGD 2011 / 平面直角座標系 XIII + 標高 (EPSG:10174)' }
 			]
 		}
 	};
