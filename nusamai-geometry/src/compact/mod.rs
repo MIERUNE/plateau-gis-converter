8304c914ae88e8db7ea8474be1e006c40a50c43d
Taku Fukada <naninunenor@gmail.com>
2024-03-26 11:46:06 +0900

    pin earcut-rs version, etc.

--- nusamai-geometry/src/compact/mod.rs
+++ nusamai-geometry/src/compact/mod.rs
@@ -4,53 +4,39 @@ mod multi_point;
 mod multi_polygon;
 mod polygon;
 
-use core::fmt::Debug;
-
 pub use linestring::{LineString, LineString2, LineString3};
 pub use multi_linestring::{MultiLineString, MultiLineString2, MultiLineString3};
 pub use multi_point::{MultiPoint, MultiPoint2, MultiPoint3};
 pub use multi_polygon::{MultiPolygon, MultiPolygon2, MultiPolygon3};
 pub use polygon::{Polygon, Polygon2, Polygon3};
-// use num_traits::{Num, NumCast};
+
+use num_traits::ToPrimitive;
 
 pub trait Coord: Clone + PartialEq {}
+pub trait CoordNum: ToPrimitive + PartialEq + Clone {}
 
-impl<const D: usize> Coord for [f64; D] {}
-impl<const D: usize> Coord for [f32; D] {}
-impl<const D: usize> Coord for [i32; D] {}
-impl<const D: usize> Coord for [i16; D] {}
-impl Coord for f64 {}
-impl Coord for f32 {}
-impl Coord for u64 {}
-impl Coord for u32 {}
+impl<const D: usize, N: CoordNum> Coord for [N; D] {}
+impl<N: CoordNum> Coord for N {}
+impl CoordNum for f32 {}
+impl CoordNum for f64 {}
+impl CoordNum for u8 {}
+impl CoordNum for i8 {}
+impl CoordNum for u16 {}
+impl CoordNum for i16 {}
+impl CoordNum for u32 {}
+impl CoordNum for i32 {}
+impl CoordNum for u64 {}
+impl CoordNum for i64 {}
 
 pub trait Coord2d: Coord {
     fn xy(&self) -> (f64, f64);
 }
 
-impl Coord2d for [f64; 2] {
+impl<N: CoordNum> Coord2d for [N; 2] {
     fn xy(&self) -> (f64, f64) {
-        (self[0], self[1])
+        (self[0].to_f64().unwrap(), self[1].to_f64().unwrap())
     }
 }
-impl Coord2d for [f32; 2] {
-    fn xy(&self) -> (f64, f64) {
-        (self[0] as f64, self[1] as f64)
-    }
-}
-impl Coord2d for [i16; 2] {
-    fn xy(&self) -> (f64, f64) {
-        (self[0] as f64, self[1] as f64)
-    }
-}
-impl Coord2d for [i32; 2] {
-    fn xy(&self) -> (f64, f64) {
-        (self[0] as f64, self[1] as f64)
-    }
-}
-
-// pub trait CoordNum: Num + Copy + NumCast + PartialOrd + Default + Debug {}
-// impl<T: Num + Copy + NumCast + PartialOrd + Default + Debug> CoordNum for T {}
 
 /// Computer-friendly Geometry
 #[cfg_attr(
@@ -68,7 +54,7 @@ pub enum Geometry<'a, T: Coord> {
 }
 
 pub type Geometry2<'a, C = f64> = Geometry<'a, [C; 2]>;
-pub type Geometry3<'a, C = f64> = Geometry<'a, [C; 2]>;
+pub type Geometry3<'a, C = f64> = Geometry<'a, [C; 3]>;
 
 #[cfg(test)]
 mod tests {
@@ -82,5 +68,15 @@ mod tests {
         // Can also be used to store integer values (e.g., vertex indices)
         let b: LineString<u32> = LineString::from_raw(vec![1, 2, 3].into());
         assert_eq!(b.len(), 3);
+    }
+
+    #[test]
+    fn coord2d() {
+        let v = [1, 2];
+        assert_eq!(v.xy(), (1.0, 2.0));
+
+        let a: LineString2<f32> =
+            LineString::from_raw(vec![[0.0, 0.0], [1.0, 1.0], [-2.0, 3.0]].into());
+        assert_eq!(a.ring_area(), 2.5);
     }
 }
