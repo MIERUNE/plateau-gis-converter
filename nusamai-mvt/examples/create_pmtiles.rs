2dcf710d7f33c2ccd77378ae13403236531d0517
nokonoko1203 <healtheworld.1203@gmail.com>
2024-06-06 17:49:22 +0900

    Added to examples

--- nusamai-mvt/examples/create_pmtiles.rs
+++ nusamai-mvt/examples/create_pmtiles.rs
@@ -1,4 +1,4 @@
-use pmtiles2::{Compression, PMTiles, TileType};
+use pmtiles2::{util::tile_id, Compression, PMTiles, TileType};
 use std::fs::{self, File};
 use std::io::Result;
 
@@ -6,15 +6,17 @@ fn main() -> Result<()> {
     let file_path = "nusamai-mvt/examples/output.pmtiles";
 
     // 新しいPMTilesアーカイブを作成
-    let mut pm_tiles = PMTiles::new(TileType::Png, Compression::GZip);
+    let mut pm_tiles = PMTiles::new(TileType::Png, Compression::None);
 
     // pngを読み込む
     let png_data = fs::read("nusamai-mvt/examples/204.png").unwrap();
 
     // タイルデータを追加
-    let _ = pm_tiles.add_tile(0, png_data);
+    let _ = pm_tiles.add_tile(tile_id(9, 445, 204), png_data);
 
-    // メタデータを書き込む
+    // タイルを読み込んでみる
+    let tile_data = pm_tiles.get_tile(445, 204, 9).unwrap();
+    println!("tile_data: {:?}", tile_data);
 
     // ファイルに書き込む
     let mut file = File::create(file_path)?;
