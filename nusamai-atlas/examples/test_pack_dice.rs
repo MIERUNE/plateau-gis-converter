f681bcb5153cb852ab35f80a8f80245f98be7782
yud0uhu <cist.b219.y.osaka@gmail.com>
2024-07-21 14:55:26 +0900

    fix: -uフラグの指定時のみ未使用ピクセルを表示するように修正

--- nusamai-atlas/examples/test_pack_dice.rs
+++ nusamai-atlas/examples/test_pack_dice.rs
@@ -3,7 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use image::{GenericImageView, Rgba};
+use image::{open, GenericImageView, Rgba};
 
 use nusamai_atlas::{
     export::PngAtlasExporter,
@@ -12,6 +12,7 @@ use nusamai_atlas::{
     texture::{DownsampleFactor, TextureCache},
 };
 
+use clap::{Arg, ArgAction, Command};
 #[derive(Debug, Clone)]
 struct Polygon {
     id: String,
@@ -210,17 +211,37 @@ fn main() {
         println!("{:?}", info);
     });
 
-    packer.finalize();
-
     let output_dir = Path::new("./examples/output/");
-    let atlas_path = output_dir.join("0.png");
     packer.export(output_dir, &texture_cache, config.width(), config.height());
 
-    let img = image::open(&atlas_path).expect("Failed to open atlas image");
-    let unused_pixels = img
-        .pixels()
-        .filter(|&(_, _, pixel)| matches!(pixel, Rgba([0, 0, 0, 0])))
-        .count();
+    let matches = Command::new("Image Processor")
+        .about("Processes images to find unused pixels")
+        .arg(
+            Arg::new("INPUT")
+                .help("Outputs unused pixels")
+                .short('u')
+                .long("unused_pixels")
+                .value_name("FILE")
+                .action(ArgAction::Set),
+        )
+        .get_matches();
 
-    println!("Unused pixels: {}", unused_pixels);
+    let input = match matches.get_one::<String>("INPUT") {
+        Some(input_value) => input_value,
+        None => {
+            std::process::exit(0);
+        }
+    };
+    let output_unused_pixels = matches.contains_id("INPUT");
+
+    let img = open(input).expect("Failed to open image");
+
+    if output_unused_pixels {
+        let unused_pixels = img
+            .pixels()
+            .filter(|&(_, _, pixel)| matches!(pixel, Rgba([0, 0, 0, 0])))
+            .count();
+
+        println!("unused pixels: {}", unused_pixels);
+    }
 }
