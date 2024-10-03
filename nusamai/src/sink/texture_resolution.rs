7b987bf256a9a80fc98efe6133d63a6c63f7552d
nokonoko1203 <healtheworld.1203@gmail.com>
2024-10-04 01:32:26 +0900

    Unify functions

--- nusamai/src/sink/texture_resolution.rs
+++ nusamai/src/sink/texture_resolution.rs
@@ -41,27 +41,8 @@ pub fn get_texture_downsample_scale_of_polygon(
 
     let pixel_coords = uv_to_pixel_coords(&uv_coords, texture_size.0, texture_size.1);
 
-    let pixel_per_distance = (0..vertices.len())
-        .map(|i| {
-            let j = (i + 1) % vertices.len();
-            let (euc0, txl0) = (
-                (vertices[i].0, vertices[i].1, vertices[i].2),
-                pixel_coords[i],
-            );
-            let (euc1, txl1) = (
-                (vertices[j].0, vertices[j].1, vertices[j].2),
-                pixel_coords[j],
-            );
-            let euc_dist =
-                ((euc0.0 - euc1.0).powi(2) + (euc0.1 - euc1.1).powi(2) + (euc0.2 - euc1.2).powi(2))
-                    .sqrt();
-            let txl_dist = ((txl0.0 as f64 - txl1.0 as f64).powi(2)
-                + (txl0.1 as f64 - txl1.1 as f64).powi(2))
-            .sqrt();
-            txl_dist / euc_dist
-        })
-        .min_by(|a, b| a.total_cmp(b))
-        .unwrap_or(1.0);
+    let vertices = vertices.iter().map(|v| (v.0, v.1, v.2)).collect::<Vec<_>>();
+    let pixel_per_distance = pixel_par_distance(&vertices, &pixel_coords);
 
     if limit_texture_resolution.unwrap_or(false) {
         1.0_f64.min(MAX_TEXTURE_PIXELS_PER_METER / pixel_per_distance)
