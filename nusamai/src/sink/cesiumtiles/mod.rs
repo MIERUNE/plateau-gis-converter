b954908e95b992f64cc643864afd95db9847477e
nokonoko1203 <healtheworld.1203@gmail.com>
2024-10-04 00:51:31 +0900

    adjust

--- nusamai/src/sink/cesiumtiles/mod.rs
+++ nusamai/src/sink/cesiumtiles/mod.rs
@@ -496,7 +496,10 @@ fn tile_writing_stage(
                             pixel_per_distance,
                             downsample_scale as f32,
                         );
-
+                        println!(
+                            "tile_zoom: {}, downsample_scale: {}, geometric_error: {}, pixel_per_distance: {}, factor: {}",
+                            tile_zoom, downsample_scale, geom_error, pixel_per_distance, factor
+                        );
                         let downsample_factor = DownsampleFactor::new(&factor);
                         let cropped_texture = PolygonMappedTexture::new(
                             &texture_uri,
@@ -725,7 +728,7 @@ fn apply_downsample_factor(
     let f = if geometric_error == 0.0 {
         1.0
     } else {
-        let f = (pixel_per_distance / (geometric_error * 1.5)).clamp(0.0, 1.0);
+        let f = (pixel_per_distance / (geometric_error / 2.0)).clamp(0.0, 1.0);
         if f.is_nan() {
             1.0
         } else {
