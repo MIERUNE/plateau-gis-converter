56cb66ba3557260f527393acde170dfbb990e40d
Yuji Mise <miseyu2000@gmail.com>
2024-10-23 17:39:37 +0900

    feat: add Address and AddressDetail structs with country information for citygml integration (#30)

--- nusamai-plateau/src/models/core.rs
+++ nusamai-plateau/src/models/core.rs
@@ -1,7 +1,24 @@
-use nusamai_citygml::citygml_data;
+use nusamai_citygml::{citygml_data, CityGmlElement};
 
 #[citygml_data(name = "core:Address")]
 #[citygml(allow_extra)]
 pub struct Address {
-    // TODO
+    #[citygml(path = b"core:xalAddress/xAL:AddressDetails")]
+    country: Vec<AddressDetail>,
+}
+
+#[citygml_data(name = "xAL:AddressDetails")]
+#[citygml(allow_extra)]
+pub struct AddressDetail {
+    #[citygml(path = b"xAL:Country")]
+    country: Option<Country>,
+}
+
+#[citygml_data(name = "xAL:Country")]
+#[citygml(allow_extra)]
+pub struct Country {
+    #[citygml(path = b"xAL:CountryName")]
+    name: Option<String>,
+    #[citygml(path = b"xAL:Locality/xAL:LocalityName")]
+    locality_name: Option<String>,
 }
