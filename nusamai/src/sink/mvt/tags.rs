d95e93846f855eecab37256c86fbd873a8e6bbdc
Taku Fukada <naninunenor@gmail.com>
2024-10-25 00:22:28 +0900

    Follow tinymvt TagsEncoder API changes (#674)

--- nusamai/src/sink/mvt/tags.rs
+++ nusamai/src/sink/mvt/tags.rs
@@ -1,42 +1,37 @@
 use nusamai_citygml::object;
 use tinymvt::tag::TagsEncoder;
 
-pub fn convert_properties(
-    tags: &mut Vec<u32>,
-    tags_enc: &mut TagsEncoder,
-    name: &str,
-    tree: &object::Value,
-) {
+pub fn convert_properties(tags_enc: &mut TagsEncoder, name: &str, tree: &object::Value) {
     match &tree {
         nusamai_citygml::Value::String(v) => {
-            tags.extend(tags_enc.add(name, v.clone().into()));
+            tags_enc.add(name, v.clone().into());
         }
         nusamai_citygml::Value::Code(v) => {
-            tags.extend(tags_enc.add(name, v.value().into()));
+            tags_enc.add(name, v.value().into());
         }
         nusamai_citygml::Value::Integer(v) => {
-            tags.extend(tags_enc.add(name, (*v).into()));
+            tags_enc.add(name, (*v).into());
         }
         nusamai_citygml::Value::NonNegativeInteger(v) => {
-            tags.extend(tags_enc.add(name, (*v).into()));
+            tags_enc.add(name, (*v).into());
         }
         nusamai_citygml::Value::Double(v) => {
-            tags.extend(tags_enc.add(name, (*v).into()));
+            tags_enc.add(name, (*v).into());
         }
         nusamai_citygml::Value::Measure(v) => {
-            tags.extend(tags_enc.add(name, v.value().into()));
+            tags_enc.add(name, v.value().into());
         }
         nusamai_citygml::Value::Boolean(v) => {
-            tags.extend(tags_enc.add(name, (*v).into()));
+            tags_enc.add(name, (*v).into());
         }
         nusamai_citygml::Value::Uri(v) => {
-            tags.extend(tags_enc.add(name, v.value().to_string().into()));
+            tags_enc.add(name, v.value().to_string().into());
         }
         nusamai_citygml::Value::Date(v) => {
-            tags.extend(tags_enc.add(name, v.to_string().into()));
+            tags_enc.add(name, v.to_string().into());
         }
         nusamai_citygml::Value::Point(v) => {
-            tags.extend(tags_enc.add(name, format!("{:?}", v).into())); // FIXME
+            tags_enc.add(name, format!("{:?}", v).into()); // FIXME
         }
         nusamai_citygml::Value::Array(_arr) => {
             // ignore non-root attributes
