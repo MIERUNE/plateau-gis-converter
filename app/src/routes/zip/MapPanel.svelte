b08aa42f1e76cde78f4f34fc30b0e4d5dbfb49ac
Taiyu Yoshizawa <yoshizawa@mierune.co.jp>
2025-09-17 09:58:38 +0900

    style修正

--- app/src/routes/zip/MapPanel.svelte
+++ app/src/routes/zip/MapPanel.svelte
@@ -475,7 +475,7 @@
 			</Popup>
 		</Marker>
 	{/if}
-	<div class="absolute top-2 left-2 z-10 flex">
+	<div class="absolute top-4 left-4 z-10 flex">
 		<button
 			data-selected={meshLevel === 'second' ? '' : undefined}
 			onclick={() => {
