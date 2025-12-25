52e2b0219832ab126eb4a49288afaa69d4c97019
Akane Kamiseto <yoshizawa@mierune.co.jp>
2025-12-25 11:27:07 +0900

    mapから変換のFeature flag 対応 (#759)
    <!-- Close or Related Issues -->
    Close #0
    
    ### What I did
    <!-- Please describe the motivation behind this PR and the changes it
    introduces. -->
    - PUBLIC_ENABLE_PLATEAU_API をtrueにすると地図からが利用できるように
    
    ### Notes
    <!-- If manual testing is required, please describe the procedure. -->

--- app/src/routes/TabNavigation.svelte
+++ app/src/routes/TabNavigation.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/state';
 	import Icon from '@iconify/svelte';
 	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { PUBLIC_ENABLE_PLATEAU_API } from '$env/static/public';
 
 	let currentPath = page.url.pathname;
 
@@ -26,13 +27,15 @@
 			<Icon icon="material-symbols:folder-zip" />
 			ZIPファイルから
 		</a>
-		<a
-			href="/map"
-			data-selected={currentPath === '/map' ? '' : undefined}
-			class="relative -ml-px inline-flex items-center gap-2 rounded-r-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-10 data-selected:pointer-events-none data-selected:bg-accent1"
-		>
-			<Icon icon="material-symbols:map" />
-			地図から
-		</a>
+		{#if PUBLIC_ENABLE_PLATEAU_API === 'true'}
+			<a
+				href="/map"
+				data-selected={currentPath === '/map' ? '' : undefined}
+				class="relative -ml-px inline-flex items-center gap-2 rounded-r-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-10 data-selected:pointer-events-none data-selected:bg-accent1"
+			>
+				<Icon icon="material-symbols:map" />
+				地図から
+			</a>
+		{/if}
 	</div>
 </nav>
