feb7317e2f35f751659abb8401b2aa31173b4295
nokonoko1203 <healtheworld.1203@gmail.com>
2026-02-05 09:00:04 +0900

    move map screen loading function to the client

--- app/src/routes/map/+page.ts
+++ app/src/routes/map/+page.ts
@@ -1,1 +1,10 @@
+import { PUBLIC_ENABLE_PLATEAU_API } from '$env/static/public';
+import { error } from '@sveltejs/kit';
+
 export const prerender = false;
+
+export const load = () => {
+	if (PUBLIC_ENABLE_PLATEAU_API !== 'true') {
+		error(404, 'Not Found');
+	}
+};
